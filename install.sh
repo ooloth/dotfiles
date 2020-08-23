d83a3e0b9b0eb389cbde776184842546ab30ded0
Michael Uloth <hello@michaeluloth.com>
2020-08-23 19:14:42 -0400

    it does not

--- install.sh
+++ install.sh
@@ -182,7 +182,7 @@ setup_ssh() {
 
   cat "$HOME/.ssh/id_rsa.pub"
 
-  vared -pr "\nType 'saved' when you've finished saving the key on GitHub. (You'll need it for the next step.) " -c reply
+  vared -p "\nType 'saved' when you've finished saving the key on GitHub. (You'll need it for the next step.) " -c reply
 
   if [ "$reply" == 'saved' ]; then
     printf "Good job! Moving on...\n"
