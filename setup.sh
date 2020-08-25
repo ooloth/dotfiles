b9c93c5c06ecac08e85b780bd630e130bb46db40
Michael Uloth <hello@michaeluloth.com>
2020-08-24 21:51:44 -0400

    extract color utils

--- setup.sh
+++ setup.sh
@@ -3,40 +3,12 @@
 # shellcheck disable=SC2154
 trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
 
+source setup/utils.sh
+
 OS_NAME=$(uname)
 COMMAND_LINE_TOOLS="/Library/Developer/CommandLineTools"
 DOTFILES="$HOME/Repos/ooloth/dotfiles"
 OH_MY_ZSH="$HOME/.oh-my-zsh"
-
-COLOR_GRAY="\033[1;38;5;243m"
-COLOR_BLUE="\033[1;34m"
-COLOR_GREEN="\033[1;32m"
-COLOR_RED="\033[1;31m"
-COLOR_PURPLE="\033[1;35m"
-COLOR_YELLOW="\033[1;33m"
-COLOR_NONE="\033[0m"
-
-title() {
-  echo -e "\n\n${COLOR_PURPLE}$1${COLOR_NONE}"
-  echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
-}
-
-error() {
-  echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
-  exit 1
-}
-
-warning() {
-  echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
-}
-
-info() {
-  echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
-}
-
-success() {
-  echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
-}
 
 confirm_plan() {
   title "Welcome to your new Mac!"
