1684467c0b023ec1507135aed71aabd00af506a6
Michael Uloth <hello@michaeluloth.com>
2022-11-04 12:41:49 -0400

    switch to startify dashboard style

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -54,7 +54,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.mode = "startify"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
