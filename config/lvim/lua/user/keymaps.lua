86645f2fb1c8f7859043261a36cc0e23c2f8fbf5
Michael Uloth <hello@michaeluloth.com>
2022-11-08 17:33:52 -0500

    comment out keymaps I'm not sure I need

--- config/lvim/lua/user/keymaps.lua
+++ config/lvim/lua/user/keymaps.lua
@@ -13,14 +13,14 @@ M.config = function()
 
   -- Editing
   -- make Y behave like D and C by yanking remainder of line only
-  vim.keymap.set("n", "Y", "y$", keymap_options)
+  -- vim.keymap.set("n", "Y", "y$", keymap_options)
 
   -- Navigate buffers
   vim.keymap.set("", "<Tab>", ":bnext<CR>", keymap_options)
   vim.keymap.set("", "<S-Tab>", ":bprevious<CR>", keymap_options)
 
-  vim.keymap.set("", "<S-l>", ":bnext<CR>", keymap_options)
-  vim.keymap.set("", "<S-h>", ":bprevious<CR>", keymap_options)
+  -- vim.keymap.set("", "<S-l>", ":bnext<CR>", keymap_options)
+  -- vim.keymap.set("", "<S-h>", ":bprevious<CR>", keymap_options)
 
   -- exit help windows with Esc or q
   vim.cmd([[
@@ -28,23 +28,26 @@ M.config = function()
     autocmd Filetype help nmap <buffer> q :q<CR>
   ]])
 
+  ---------------
+  -- TELESCOPE --
+  ---------------
   -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
   -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
-  -- local _, actions = pcall(require, "telescope.actions")
-  -- lvim.builtin.telescope.defaults.mappings = {
-  --   -- for input mode
-  --   i = {
-  --     ["<C-j>"] = actions.move_selection_next,
-  --     ["<C-k>"] = actions.move_selection_previous,
-  --     ["<C-n>"] = actions.cycle_history_next,
-  --     ["<C-p>"] = actions.cycle_history_prev,
-  --   },
-  --   -- for normal mode
-  --   n = {
-  --     ["<C-j>"] = actions.move_selection_next,
-  --     ["<C-k>"] = actions.move_selection_previous,
-  --   },
-  -- }
+  local _, actions = pcall(require, "telescope.actions")
+  lvim.builtin.telescope.defaults.mappings = {
+    -- for input mode
+    i = {
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+      ["<C-n>"] = actions.cycle_history_next,
+      ["<C-p>"] = actions.cycle_history_prev,
+    },
+    -- for normal mode
+    n = {
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+    },
+  }
 end
 
 return M
