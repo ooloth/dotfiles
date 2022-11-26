e92a48b13f5f0e8056986ea9334e2536647a881a
Michael Uloth <hello@michaeluloth.com>
2022-11-25 23:30:40 -0500

    replace " with '

--- config/nvim/lua/mu/core/keymaps.lua
+++ config/nvim/lua/mu/core/keymaps.lua
@@ -17,20 +17,20 @@ vim.keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>')
 ----------------
 
 -- search
-vim.keymap.set("n", "<leader>nh", ":nohl<CR>") -- clear search highlights
+vim.keymap.set('n', '<leader>nh', ':nohl<CR>') -- clear search highlights
 
 -- numbers
 vim.keymap.set('n', '<leader>+', '<C-a>') -- increment number under cursor
 vim.keymap.set('n', '<leader>-', '<C-x>') -- decrement number under cursor
 
 -- windows 
-vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+vim.keymap.set('n', '<leader>sv', '<C-w>v') -- split window vertically
+vim.keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=') -- make split windows equal width & height
+vim.keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
 
 -- tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>') -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>') -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>') --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>') --  go to previous tab
