4a44954a8fde9a30cd14be02f076addebd0fad62
Michael Uloth <hello@michaeluloth.com>
2022-11-27 17:40:25 -0500

    add snippets to autocompletion

--- config/nvim/lua/mu/plugins-setup.lua
+++ config/nvim/lua/mu/plugins-setup.lua
@@ -57,6 +57,11 @@ return packer.startup(function(use)
   use("hrsh7th/cmp-buffer") -- source for text in buffer
   use("hrsh7th/cmp-path") -- source for file system paths
 
+  -- snippets
+  use("L3MON4D3/LuaSnip") -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- autocomplete snippets with nvim-cmp
+  use("rafamadriz/friendly-snippets") -- useful snippets
+
   -- if packer was just installed, sync plugins
   if packer_bootstrap then
     require('packer').sync()
