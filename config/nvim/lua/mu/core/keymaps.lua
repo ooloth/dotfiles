9568c4ffae6af2362c96ad66898606e7853ebcb4
Michael Uloth <hello@michaeluloth.com>
2022-11-27 15:26:22 -0500

    sort leader keymaps alphabetically

--- config/nvim/lua/mu/core/keymaps.lua
+++ config/nvim/lua/mu/core/keymaps.lua
@@ -1,67 +1,67 @@
 vim.g.mapleader = ' '
 
--------------
--- GENERAL --
--------------
+--------------------
+-- NON-LEADER KEY --
+--------------------
 
+-- command mode
 vim.keymap.set({ 'n', 'v' }, ',', ':') -- enter command mode with , instead of :
-vim.keymap.set({ 'n', 'v' }, ':', ',') -- walk through f and t results using ;/: (like n/N for / results)
-vim.keymap.set('n', 'x', '"_x"') -- delete single character without copying into register 
+
+-- navigation
+vim.keymap.set('n', '<TAB>', ':bnext<CR>') -- go to next buffer
+vim.keymap.set('n', '<S-TAB>', ':bprev<CR>') -- go to previous buffer
+vim.keymap.set({ 'n', 'v' }, ':', ',') -- navigate f and t results using ;/: (like n/N for / results)
 
 -------------
 -- PLUGINS --
 -------------
 
--- vim-maximizer
-vim.keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>')
-
--- nvim-tree
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
-
--- telescope
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>') -- find files within current working directory, respects .gitignore
-vim.keymap.set('n', '<leader>fs', '<cmd>Telescope live_grep<CR>') -- find string in current working directory as you type
-vim.keymap.set('n', '<leader>fc', '<cmd>Telescope grep_string<CR>') -- find string under cursor in current working directory
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>') -- list open buffers in current neovim instance
-vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>') -- list available help tags
-
--- telescope git commands (not on youtube nvim video)
-vim.keymap.set('n', '<leader>gc', '<cmd>Telescope git_commits<CR>') -- list all git commits (use <CR> to checkout) ['gc' for git commits]
-vim.keymap.set('n', '<leader>gfc', '<cmd>Telescope git_bcommits<CR>') -- list git commits for current file/buffer (use <CR> to checkout) ['gfc' for git file commits]
-vim.keymap.set('n', '<leader>gb', '<cmd>Telescope git_branches<CR>') -- list git branches (use <CR> to checkout) ['gb' for git branch]
-vim.keymap.set('n', '<leader>gs', '<cmd>Telescope git_status<CR>') -- list current changes per file with diff preview ['gs' for git status]
-
--- restart lsp server (not on youtube nvim video)
-vim.keymap.set('n', '<leader>rs', ':LspRestart<CR>') -- mapping to restart lsp if necessary
-
 ----------------
 -- LEADER KEY --
 ----------------
 
--- general
-vim.keymap.set('n', '<leader>w', ':w<CR>') -- save current buffer 
-
--- search
-vim.keymap.set('n', '<leader>nh', ':nohl<CR>') -- clear search highlights
-
--- numbers
+-- symbols
 vim.keymap.set('n', '<leader>+', '<C-a>') -- increment number under cursor
 vim.keymap.set('n', '<leader>-', '<C-x>') -- decrement number under cursor
 
--- splits 
+-- e (explore)
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>') -- view project file tree 
+
+-- f (find)
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>') -- find open buffers 
+vim.keymap.set('n', '<leader>fc', '<cmd>Telescope grep_string<CR>') -- find string under cursor in cwd
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>') -- fuzzy-find files in cwd (respects .gitignore)
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>') -- find available help tags
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope live_grep<CR>') -- fuzzy-find string in cwd
+
+-- g (git)
+vim.keymap.set('n', '<leader>gb', '<cmd>Telescope git_branches<CR>') -- list branches (use <CR> to checkout)
+vim.keymap.set('n', '<leader>gc', '<cmd>Telescope git_commits<CR>') -- list all commits (use <CR> to checkout)
+vim.keymap.set('n', '<leader>gfc', '<cmd>Telescope git_bcommits<CR>') -- list file commits (use <CR> to checkout) 
+vim.keymap.set('n', '<leader>gs', '<cmd>Telescope git_status<CR>') -- list current changes per file with diff preview 
+
+-- h (no highlight)
+vim.keymap.set('n', '<leader>h', ':nohl<CR>') -- no search highlights
+
+-- l (lsp)
+vim.keymap.set('n', '<leader>lr', ':LspRestart<CR>') -- restart lsp 
+
+-- s (split)
 vim.keymap.set('n', '<leader>sv', '<C-w>v') -- split window vertically
 vim.keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
-vim.keymap.set('n', '<leader>se', '<C-w>=') -- make split windows equal width & height
-vim.keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
+vim.keymap.set('n', '<leader>se', '<C-w>=') -- make splits equal width & height
+vim.keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>') -- maximize split / restore split layout with vim-maximizer
+vim.keymap.set('n', '<leader>sx', ':close<CR>') -- close current split (or use ZZ/ZQ)
 
--- windows
-vim.keymap.set('n', '<leader>x', '<C-w>x') -- close window (but keep buffer)
-
--- buffers
--- vim.keymap.set('n', '<leader>x', ':bd') -- close 
-
--- tabs
+-- t (tab)
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>') -- go to next tab
 vim.keymap.set('n', '<leader>to', ':tabnew<CR>') -- open new tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>') -- go to previous tab
 vim.keymap.set('n', '<leader>tx', ':tabclose<CR>') -- close current tab
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>') --  go to next tab
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>') --  go to previous tab
+
+-- w (save)
+vim.keymap.set('n', '<leader>w', ':w<CR>') -- save current buffer 
+
+-- x (close)
+vim.keymap.set('n', '<leader>x', ':bd<CR>') -- close 
+
