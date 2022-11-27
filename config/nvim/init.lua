2c0802169e4aa2cbe24a7a99f62a2adb916a31bc
Michael Uloth <hello@michaeluloth.com>
2022-11-27 18:15:11 -0500

    add mason and lsp servers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,10 @@
 require('mu.plugins-setup') -- should be first
-require('mu.core.options')
-require('mu.core.keymaps')
 require('mu.core.colorscheme')
+require('mu.core.keymaps')
+require('mu.core.options')
 require('mu.plugins.comment')
+require('mu.plugins.lsp.mason')
 require('mu.plugins.lualine')
+require('mu.plugins.nvim-cmp')
 require('mu.plugins.nvim-tree')
 require('mu.plugins.telescope')
-require('mu.plugins.nvim-cmp')
--- config/nvim/lua/mu/plugins-setup.lua
+++ config/nvim/lua/mu/plugins-setup.lua
@@ -62,6 +62,17 @@ return packer.startup(function(use)
   use('saadparwaiz1/cmp_luasnip') -- add snippets to autocompletions
   use('rafamadriz/friendly-snippets') -- useful snippets
 
+  -- managing & installing lsp servers, linters & formatters
+  use('williamboman/mason.nvim') -- in charge of managing lsp servers, linters & formatters
+  use('williamboman/mason-lspconfig.nvim') -- bridges gap b/w mason & lspconfig
+
+  -- configuring lsp servers
+  use('neovim/nvim-lspconfig') -- easily configure language servers
+  use('hrsh7th/cmp-nvim-lsp') -- add lsp server info to autocompletions
+  use({ 'glepnir/lspsaga.nvim', branch = 'main' }) -- enhanced lsp uis
+  use('jose-elias-alvarez/typescript.nvim') -- additional functionality for typescript server (e.g. rename file & update imports)
+  use('onsails/lspkind.nvim') -- vs-code like icons for autocompletion
+
   -- if packer was just installed, sync plugins
   if packer_bootstrap then
     require('packer').sync()
