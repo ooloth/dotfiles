77272256bd19134dbafaaf752c1be9cf51e80085
Michael Uloth <hello@michaeluloth.com>
2022-11-27 18:14:28 -0500

    change " to '

--- config/nvim/lua/mu/plugins-setup.lua
+++ config/nvim/lua/mu/plugins-setup.lua
@@ -1,9 +1,9 @@
 -- automatically install packer if not installed
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd([[packadd packer.nvim]])
     return true
   end
@@ -21,46 +21,46 @@ vim.cmd([[
 ]])
 
 -- import packer safely
-local status, packer = pcall(require, "packer")
+local status, packer = pcall(require, 'packer')
 if not status then
   return
 end
 
 -- install plugins
 return packer.startup(function(use)
-  use("wbthomason/packer.nvim") -- packer can manage itself
+  use('wbthomason/packer.nvim') -- packer can manage itself
 
   -- utilities
-  use("nvim-lua/plenary.nvim") -- lua functions many other plugins use
+  use('nvim-lua/plenary.nvim') -- lua functions many other plugins use
 
   -- colors
-  use("bluz71/vim-nightfly-guicolors") 
+  use('bluz71/vim-nightfly-guicolors') 
 
   -- navigating
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- telescope dependency for faster sorting 
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
-  use("nvim-tree/nvim-tree.lua") -- file explorer
-  use("nvim-tree/nvim-web-devicons") -- vs code-like icons
-  use("christoomey/vim-tmux-navigator") -- navigate vim splits (and tmux panes) with <C-hjkl>
-  use("szw/vim-maximizer") -- maximizes and restores the current split
+  use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }) -- telescope dependency for faster sorting 
+  use({ 'nvim-telescope/telescope.nvim', branch = '0.1.x' }) -- fuzzy finder
+  use('nvim-tree/nvim-tree.lua') -- file explorer
+  use('nvim-tree/nvim-web-devicons') -- vs code-like icons
+  use('christoomey/vim-tmux-navigator') -- navigate vim splits (and tmux panes) with <C-hjkl>
+  use('szw/vim-maximizer') -- maximizes and restores the current split
 
   -- editing
-  use("tpope/vim-surround") -- add, delete, change surrounding characters
-  use("inkarkat/vim-ReplaceWithRegister") -- replace text with register contents (gr + motion)
-  use("numToStr/Comment.nvim") -- comment text (gc + motion)
+  use('tpope/vim-surround') -- add, delete, change surrounding characters
+  use('inkarkat/vim-ReplaceWithRegister') -- replace text with register contents (gr + motion)
+  use('numToStr/Comment.nvim') -- comment text (gc + motion)
 
   -- status
-  use("nvim-lualine/lualine.nvim") -- statusline
+  use('nvim-lualine/lualine.nvim') -- statusline
 
-   -- autocompletion
-  use("hrsh7th/nvim-cmp") -- completion plugin
-  use("hrsh7th/cmp-buffer") -- source for text in buffer
-  use("hrsh7th/cmp-path") -- source for file system paths
+  -- autocompletion
+  use('hrsh7th/nvim-cmp') -- completion plugin
+  use('hrsh7th/cmp-buffer') -- source for text in buffer
+  use('hrsh7th/cmp-path') -- source for file system paths
 
   -- snippets
-  use("L3MON4D3/LuaSnip") -- snippet engine
-  use("saadparwaiz1/cmp_luasnip") -- autocomplete snippets with nvim-cmp
-  use("rafamadriz/friendly-snippets") -- useful snippets
+  use('L3MON4D3/LuaSnip') -- snippet engine
+  use('saadparwaiz1/cmp_luasnip') -- add snippets to autocompletions
+  use('rafamadriz/friendly-snippets') -- useful snippets
 
   -- if packer was just installed, sync plugins
   if packer_bootstrap then
