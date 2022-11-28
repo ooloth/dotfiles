61c99f365c6ec38ac657dab47cfdd14b1fbc05ae
Michael Uloth <hello@michaeluloth.com>
2022-11-28 09:56:52 -0500

    use nightfly colors in lsp floating windows

--- config/nvim/lua/mu/core/colorscheme.lua
+++ config/nvim/lua/mu/core/colorscheme.lua
@@ -3,3 +3,23 @@ if not status then
   print('Colorscheme not found!')
   return
 end
+
+-- style floating windows
+-- See: https://github.com/bluz71/vim-nightfly-colors#nightflynormalfloat
+vim.g.nightflyNormalFloat = true
+
+-- style lsp floating windows
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = "single"
+  }
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signatureHelp, {
+    border = "single"
+  }
+)
+
+vim.diagnostic.config({ float = { border = "single" } })
+
