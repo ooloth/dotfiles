8abb1a080367d9de8c2cb32152b850bedb80427a
Michael Uloth <hello@michaeluloth.com>
2022-11-28 09:58:33 -0500

    use nightfly colors in tmux

--- config/nvim/lua/mu/core/colorscheme.lua
+++ config/nvim/lua/mu/core/colorscheme.lua
@@ -23,3 +23,8 @@ vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
 
 vim.diagnostic.config({ float = { border = "single" } })
 
+-- display correct colors when vim opened inside tmux
+vim.cmd([[ 
+  let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+  let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum" 
+]])
