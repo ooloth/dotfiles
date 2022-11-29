d0492e6f9f01c94702c824d2b6804dadde39304d
Michael Uloth <hello@michaeluloth.com>
2022-11-29 15:36:48 -0500

    add more parsers to ensure list

--- config/nvim/lua/mu/plugins/treesitter.lua
+++ config/nvim/lua/mu/plugins/treesitter.lua
@@ -6,32 +6,42 @@ end
 
 -- configure treesitter
 treesitter.setup({
+  -- install missing parsers when entering buffer
+  auto_install = true,
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
+  -- ensure these language parsers are installed
+  ensure_installed = {
+    'bash',
+    'comment',
+    'css',
+    'diff',
+    'dockerfile',
+    'gitcommit',
+    -- 'gitignore', -- currently errors
+    'graphql',
+    'help',
+    'html',
+    'javascript',
+    'jsdoc',
+    'json',
+    'json5',
+    'jsonc',
+    'lua',
+    'markdown',
+    'pug',
+    'python',
+    'svelte',
+    'toml',
+    'tsx',
+    'typescript',
+    'vim',
+    'yaml',
+  },
   -- enable syntax highlighting
   highlight = {
     enable = true,
   },
   -- enable indentation
   indent = { enable = true },
-  -- enable autotagging (w/ nvim-ts-autotag plugin)
-  autotag = { enable = true },
-  -- ensure these language parsers are installed
-  ensure_installed = {
-    'bash',
-    'css',
-    'dockerfile',
-    'gitignore',
-    'graphql',
-    'html',
-    'javascript',
-    'json',
-    'lua',
-    'markdown',
-    'svelte',
-    'tsx',
-    'typescript',
-    'vim',
-    'yaml',
-  },
-  -- auto install above language parsers
-  auto_install = true,
 })
