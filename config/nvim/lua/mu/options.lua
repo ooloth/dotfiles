b90c9f1d28c66cfd07e5ddc6ae93d46885d93e15
Michael Uloth <hello@michaeluloth.com>
2022-12-06 15:02:31 -0500

    enable confirm + reorganize option groupings

--- config/nvim/lua/mu/options.lua
+++ config/nvim/lua/mu/options.lua
@@ -1,38 +1,45 @@
--- line numbers
+---------------
+-- BEHAVIOUR --
+---------------
+
+-- editing
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.clipboard:append('unnamedplus') -- use the macos system clipboard when yanking, cutting or deleting
+vim.opt.iskeyword:append('-') -- treat hyphens as part of a single word
+
+-- searching
+vim.opt.ignorecase = true -- assume I want a case-insensitive search if my search only includes lowercase characters
+vim.opt.smartcase = true -- assume I want a case-sensitive search if my search includes uppercase characters
+
+-- quitting
+vim.opt.confirm = true -- offer to save changes to open files before :q
+
+----------------
+-- APPEARANCE --
+----------------
+
+-- colors
+vim.opt.termguicolors = true
+vim.opt.background = 'dark' -- default light/dark colorschemes to dark
+
+-- cursor
+vim.opt.cursorline = true
+
+-- lines
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.wrap = false
+
+-- spacing
+vim.opt.cmdheight = 0
+vim.opt.signcolumn = 'yes'
+
+-- split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 -- tabs & indentation
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-
--- line wrapping
-vim.opt.wrap = false
-
--- search
-vim.opt.ignorecase = true -- assume I want a case-insensitive search if my search only includes lowercase characters
-vim.opt.smartcase = true -- assume I want a case-sensitive search if my search includes uppercase characters
-
--- cursor line
-vim.opt.cursorline = true
-
--- appearance
-vim.opt.termguicolors = true
-vim.opt.background = 'dark' -- default light/dark colorschemes to dark
-vim.opt.signcolumn = 'yes'
-vim.opt.cmdheight = 0
-
--- backspace
-vim.opt.backspace = 'indent,eol,start'
-
--- clipboard
-vim.opt.clipboard:append('unnamedplus') -- use the macos system clipboard when yanking, cutting or deleting
-
--- split windows
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- editing
-vim.opt.iskeyword:append('-') -- treat hyphens as part of a single word
