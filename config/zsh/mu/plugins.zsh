0f7d3e416ae8dfff758790c8cc3cac91c3f31a78
Michael Uloth <hello@michaeluloth.com>
2022-12-06 17:34:01 -0500

    condense prompt section

--- config/zsh/mu/plugins.zsh
+++ config/zsh/mu/plugins.zsh
@@ -4,9 +4,7 @@ eval "$(fnm env --use-on-cd)"
 # prompt
 # eval "$(starship init zsh)"
 source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme
-
-# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
-source $HOME/.config/zsh/mu/.p10k.zsh
+source $HOME/.config/zsh/mu/.p10k.zsh # to customize, run `p10k configure` or edit config/zsh/mu/.p10k.zsh
 
 # zsh-autosuggestions
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
