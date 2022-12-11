926b29ddd2e57bc324474d0f2c60b1f7debdaa6e
Michael Uloth <hello@michaeluloth.com>
2022-12-11 00:19:46 -0500

    delete unused settings

--- config/nvim/lua/mu/colorscheme.lua
+++ config/nvim/lua/mu/colorscheme.lua
@@ -1,19 +1,12 @@
-require('mu.plugins.colorscheme.dracula') -- require relative to init.lua
--- require('mu.plugins.colorscheme.nightfly') -- require relative to init.lua
-
--- style lsp floating windows
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = 'single',
-})
-
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signatureHelp, {
-  border = 'single',
-})
-
-vim.diagnostic.config({ float = { border = 'single' } })
+require('mu.plugins.colorscheme.dracula') -- require is relative to init.lua
 
 -- display correct colors when vim opened inside tmux
-vim.cmd([[ 
+vim.cmd([[
   let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
-  let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum" 
+  let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+]])
+
+-- highlight MDX like MD
+vim.cmd([[
+  autocmd BufNewFile,BufRead *.mdx set filetype=markdown.mdx
 ]])
