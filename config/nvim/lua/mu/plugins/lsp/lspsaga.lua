b03caca036692c6fa2b41fc61b89b8b7f8ce1b2c
Michael Uloth <hello@michaeluloth.com>
2022-12-11 00:13:07 -0500

    show source of each diagnostic

--- config/nvim/lua/mu/plugins/lsp/lspsaga.lua
+++ config/nvim/lua/mu/plugins/lsp/lspsaga.lua
@@ -3,7 +3,11 @@ if not lspsaga_ok then
   return
 end
 
+-- float source currently only shows in lspsaga jump window
+vim.diagnostic.config({ float = { source = true }, virtual_text = { source = true } })
+
 lspsaga.init_lsp_saga({
+  border_style = 'rounded',
   definition_action_keys = { edit = '<cr>' }, -- use enter to open file with definition preview
   finder_action_keys = { open = '<cr>' }, -- use enter to open file with finder
   move_in_saga = { prev = '<C-k>', next = '<C-j>' }, -- keybinds for navigation in lspsaga window
