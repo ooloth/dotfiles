9bc05334597975e090382485f7a983d452c3283d
Michael Uloth <hello@michaeluloth.com>
2022-12-14 18:23:25 -0500

    remove non-working autocmd

--- config/nvim/lua/mu/options.lua
+++ config/nvim/lua/mu/options.lua
@@ -16,20 +16,6 @@ vim.opt.smartcase = true -- unless search includes uppercase letters
 -- quitting
 vim.opt.confirm = true -- offer to save changes to open files before :q
 
--- see: https://unix.stackexchange.com/a/383044
-vim.cmd([[
-" Trigger `autoread` when files changes on disk
-" https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
-" https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
-    autocmd FocusGained,BufEnter,CursorHold,CursorHoldI *
-            \ if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif
-
-" Notification after file change
-" https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
-autocmd FileChangedShellPost *
-  \ echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
-]])
-
 ----------------
 -- APPEARANCE --
 ----------------
