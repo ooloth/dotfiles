e5a84770093ce44a1e362c4a3b16949012d39eac
Michael Uloth <hello@michaeluloth.com>
2022-12-21 16:56:55 -0500

    remove the unnecessary progress indicator in inactive windows

--- config/nvim/lua/mu/plugins/lualine.lua
+++ config/nvim/lua/mu/plugins/lualine.lua
@@ -12,6 +12,7 @@ lualine.setup({
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
+    -- the larger the shorting_target number, the sooner the file path abbreviates
     -- see: https://github.com/nvim-lualine/lualine.nvim#filename-component-options
     lualine_c = { { 'filename', path = 1, shorting_target = 100 } },
     lualine_x = { 'filetype' },
@@ -21,6 +22,5 @@ lualine.setup({
   inactive_sections = {
     -- see: https://github.com/nvim-lualine/lualine.nvim#filename-component-options
     lualine_c = { { 'filename', path = 1, shorting_target = 100 } },
-    lualine_y = { 'progress' },
   },
 })
