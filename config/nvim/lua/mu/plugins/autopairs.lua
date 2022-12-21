79359b0eba98f69799654ef5c3c63c5acfe7387c
Michael Uloth <hello@michaeluloth.com>
2022-12-21 16:57:18 -0500

    disable autopairs exceptions

--- config/nvim/lua/mu/plugins/autopairs.lua
+++ config/nvim/lua/mu/plugins/autopairs.lua
@@ -17,8 +17,8 @@ end
 autopairs.setup({
   check_ts = true, -- enable treesitter
   ts_config = {
-    lua = { 'string' }, -- don't add pairs in lua string treesitter nodes
-    javascript = { 'template_string' }, -- don't add pairs in javscript template_string treesitter nodes
+    -- lua = { 'string' }, -- don't add pairs in lua string treesitter nodes
+    -- javascript = { 'template_string' }, -- don't add pairs in javscript template_string treesitter nodes
   },
 })
 
