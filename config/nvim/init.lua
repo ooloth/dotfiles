55a855812c049fc1d85edb2705f3bf1ae0a02984
Michael Uloth <hello@michaeluloth.com>
2023-02-13 22:37:21 -0700

    conditionally load my config or lazyvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,16 +1,22 @@
-require('mu.plugins-setup') -- should be first
-require('mu.colorscheme')
-require('mu.keymaps')
-require('mu.options')
-require('mu.plugins.autopairs')
-require('mu.plugins.comment')
-require('mu.plugins.gitsigns')
-require('mu.plugins.lsp.lspsaga') -- set up before mason
-require('mu.plugins.lsp.mason')
-require('mu.plugins.lualine')
-require('mu.plugins.nvim-cmp')
-require('mu.plugins.nvim-tree')
-require('mu.plugins.telescope')
-require('mu.plugins.treesitter')
-require('mu.plugins.trouble')
-require('mu.plugins.vim-floaterm')
+local config = 'mine' -- 'mine' or 'lazyvim'
+
+if config == 'lazyvim' then
+  require('config.lazyvim')
+else
+  require('mu.plugins-setup') -- should be first
+  require('mu.colorscheme')
+  require('mu.keymaps')
+  require('mu.options')
+  require('mu.plugins.autopairs')
+  require('mu.plugins.comment')
+  require('mu.plugins.gitsigns')
+  require('mu.plugins.lsp.lspsaga') -- set up before mason
+  require('mu.plugins.lsp.mason')
+  require('mu.plugins.lualine')
+  require('mu.plugins.nvim-cmp')
+  require('mu.plugins.nvim-tree')
+  require('mu.plugins.telescope')
+  require('mu.plugins.treesitter')
+  require('mu.plugins.trouble')
+  require('mu.plugins.vim-floaterm')
+end
