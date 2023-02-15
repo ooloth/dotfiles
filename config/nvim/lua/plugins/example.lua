385df27f0dac3720c502af1fb35899d7afb79208
Michael Uloth <hello@michaeluloth.com>
2023-02-14 22:55:24 -0700

    delete disabled example

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -10,48 +10,45 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  { 'ellisonleao/gruvbox.nvim' },
 
   -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
+    'LazyVim/LazyVim',
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = 'gruvbox',
     },
   },
 
   -- change trouble config
   {
-    "folke/trouble.nvim",
+    'folke/trouble.nvim',
     -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
   },
 
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
-
   -- add symbols-outline
   {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    'simrat39/symbols-outline.nvim',
+    cmd = 'SymbolsOutline',
+    keys = { { '<leader>cs', '<cmd>SymbolsOutline<cr>', desc = 'Symbols Outline' } },
     config = true,
   },
 
   -- override nvim-cmp and add cmp-emoji
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-emoji' },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      local cmp = require('cmp')
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = 'emoji' } }))
     end,
   },
 
   -- change some telescope options and a keymap to browse plugin files
   {
-    "nvim-telescope/telescope.nvim",
+    'nvim-telescope/telescope.nvim',
     keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
@@ -64,9 +61,9 @@ return {
     -- change some options
     opts = {
       defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
+        layout_strategy = 'horizontal',
+        layout_config = { prompt_position = 'top' },
+        sorting_strategy = 'ascending',
         winblend = 0,
       },
     },
@@ -74,19 +71,19 @@ return {
 
   -- add telescope-fzf-native
   {
-    "telescope.nvim",
+    'telescope.nvim',
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
       config = function()
-        require("telescope").load_extension("fzf")
+        require('telescope').load_extension('fzf')
       end,
     },
   },
 
   -- add pyright to lspconfig
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
@@ -99,14 +96,14 @@ return {
 
   -- add tsserver and setup with typescript.nvim instead of lspconfig
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
+      'jose-elias-alvarez/typescript.nvim',
       init = function()
-        require("lazyvim.util").on_attach(function(_, buffer)
+        require('lazyvim.util').on_attach(function(_, buffer)
           -- stylua: ignore
           vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+          vim.keymap.set('n', '<leader>cR', 'TypescriptRenameFile', { desc = 'Rename File', buffer = buffer })
         end)
       end,
     },
@@ -123,7 +120,7 @@ return {
       setup = {
         -- example to setup with typescript.nvim
         tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
+          require('typescript').setup({ server = opts })
           return true
         end,
         -- Specify * to use this function as a fallback for any server
@@ -134,28 +131,28 @@ return {
 
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = 'lazyvim.plugins.extras.lang.typescript' },
 
   -- add more treesitter parsers
   {
-    "nvim-treesitter/nvim-treesitter",
+    'nvim-treesitter/nvim-treesitter',
     opts = {
       ensure_installed = {
-        "bash",
-        "help",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
+        'bash',
+        'help',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'regex',
+        'tsx',
+        'typescript',
+        'vim',
+        'yaml',
       },
     },
   },
@@ -164,29 +161,29 @@ return {
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
   {
-    "nvim-treesitter/nvim-treesitter",
+    'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
-          "tsx",
-          "typescript",
+        'tsx',
+        'typescript',
       })
     end,
   },
 
   -- the opts function can also be used to change the default opts:
   {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
     opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
+      table.insert(opts.sections.lualine_x, '😄')
     end,
   },
 
   -- or you can return new options to override all the defaults
   {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
     opts = function()
       return {
         --[[add your custom lualine config here]]
@@ -195,20 +192,20 @@ return {
   },
 
   -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  { import = 'lazyvim.plugins.extras.ui.mini-starter' },
 
   -- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = 'lazyvim.plugins.extras.lang.json' },
 
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     opts = {
       ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
+        'stylua',
+        'shellcheck',
+        'shfmt',
+        'flake8',
       },
     },
   },
@@ -216,30 +213,30 @@ return {
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
-    "L3MON4D3/LuaSnip",
+    'L3MON4D3/LuaSnip',
     keys = function()
       return {}
     end,
   },
   -- then: setup supertab in cmp
   {
-    "hrsh7th/nvim-cmp",
+    'hrsh7th/nvim-cmp',
     dependencies = {
-      "hrsh7th/cmp-emoji",
+      'hrsh7th/cmp-emoji',
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
       end
 
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
+      local luasnip = require('luasnip')
+      local cmp = require('cmp')
 
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
+      opts.mapping = vim.tbl_extend('force', opts.mapping, {
+        ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
@@ -251,8 +248,8 @@ return {
           else
             fallback()
           end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
@@ -260,7 +257,7 @@ return {
           else
             fallback()
           end
-        end, { "i", "s" }),
+        end, { 'i', 's' }),
       })
     end,
   },
