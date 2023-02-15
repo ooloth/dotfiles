b4d14da6438b5c6caecbae0f22228c555c34ad1a
Michael Uloth <hello@michaeluloth.com>
2023-02-15 00:11:14 -0700

    customize lualine

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,13 @@
 return {
   -- add dracula
-  { 'Mofiqul/dracula.nvim' },
+  {
+    'Mofiqul/dracula.nvim',
+    -- See: https://github.com/Mofiqul/dracula.nvim#-configuration
+    opts = {
+      italic_comment = true,
+      lualine_bg_color = '#282A36',
+    },
+  },
 
   -- Configure LazyVim to load dracula
   { 'LazyVim/LazyVim', opts = { colorscheme = 'dracula' } },
