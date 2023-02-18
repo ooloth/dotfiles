09d7a9f0964fb683aeac7bbbbf35a0b6fac1b942
Michael Uloth <hello@michaeluloth.com>
2023-02-18 12:03:42 -0700

    comment out migrated settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,12 +4,12 @@ if config == 'lazyvim' then
   require('config.lazy')
 else
   require('mu.plugins-setup') -- should be first
-  require('mu.colorscheme')
+  -- require('mu.colorscheme')
   require('mu.keymaps')
-  require('mu.options')
-  require('mu.plugins.autopairs')
-  require('mu.plugins.comment')
-  require('mu.plugins.gitsigns')
+  -- require('mu.options')
+  -- require('mu.plugins.autopairs')
+  -- require('mu.plugins.comment')
+  -- require('mu.plugins.gitsigns')
   require('mu.plugins.lsp.lspsaga') -- set up before mason
   require('mu.plugins.lsp.mason')
   require('mu.plugins.lualine')
