ff041cb3d00794b4357f37b73db5d0766bdfb1d3
Michael Uloth <hello@michaeluloth.com>
2023-02-21 16:37:53 -0500

    add comments

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,18 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- checkhealth settings
+-- quieter checkhealth
 vim.cmd([[
   let g:loaded_perl_provider = 0
   let g:loaded_ruby_provider = 0
 ]])
 
+-- python
 if vim.env.IS_WORK_LAPTOP == 'true' then
   vim.cmd([[
     let g:python3_host_prog = '$HOME/.pyenv/versions/py3nvim/bin/python'
   ]])
 end
 
+-- UI
 vim.opt.cmdheight = 0
 vim.opt.number = false
 vim.opt.relativenumber = false
