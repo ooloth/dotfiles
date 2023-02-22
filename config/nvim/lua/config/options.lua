43775a6621ce52e8ebdb54a100b41a4024d71942
Michael Uloth <hello@michaeluloth.com>
2023-02-21 21:20:13 -0500

    update comments

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,18 +2,19 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- checkhealth
+vim.cmd([[
+  let g:loaded_perl_provider = 0
+  let g:loaded_ruby_provider = 0
+]])
+
+-- floating windows
 vim.diagnostic.config({
   float = { border = 'rounded', source = 'always' },
   severity_sort = true,
   -- update_in_insert = true,
   virtual_text = { source = 'always' },
 })
-
--- quieter checkhealth
-vim.cmd([[
-  let g:loaded_perl_provider = 0
-  let g:loaded_ruby_provider = 0
-]])
 
 -- python
 if vim.env.IS_WORK_LAPTOP == 'true' then
