ee5f58f107c9132cadd9bcdabf7cfe1130d85dc4
Michael Uloth <hello@michaeluloth.com>
2023-02-22 16:00:02 -0500

    short comments

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
 -- checkhealth
