b2b537ed660791b18f113b8292216c2138848d26
Michael Uloth <hello@michaeluloth.com>
2023-02-22 16:32:32 -0500

    toggle neo-tree with leader-e

---
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    keys = function() -- replace all default keys
+      return {
+        {
+          '<leader>fe',
+          function()
+            -- see: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/editor.lua
+            require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
+          end,
+          desc = 'Explorer',
+        },
+        { '<leader>e', '<leader>fe', desc = 'Explorer', remap = true },
+      }
+    end,
+  },
+}
