8fa6e8fdbf1eb4c8b4f26e51b74ce98bf3cdfdae
Michael Uloth <hello@michaeluloth.com>
2023-02-21 21:20:24 -0500

    remove duplicates

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -9,22 +9,9 @@ return {
           vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
         end
 
-      map('n', ']h', gs.next_hunk, 'Next hunk')
-      map('n', '[h', gs.prev_hunk, 'Prev hunk')
-      map('n', '<leader>gb', gs.blame_line, 'Blame (line)')
-      map('n', '<leader>gd', gs.diffthis, 'Diff vs index (file)')
-      map('n', '<leader>gD', function()
-        gs.diffthis('~')
-      end, 'Diff vs ~ (file)')
-      map('n', '<leader>gp', gs.preview_hunk, 'Preview (hunk)')
-      map('n', '<leader>gtb', gs.toggle_current_line_blame, 'Blame')
-      map('n', '<leader>gtd', gs.toggle_deleted, 'Deleted lines')
-      -- make hunks available as text objects
-      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<cr>', 'Select hunk')
-    end,
         map('n', ']h', gs.next_hunk, 'Next hunk')
         map('n', '[h', gs.prev_hunk, 'Prev hunk')
-        map('n', '<leader>gb', gs.blame_line, 'Blame (line)')
+        map('n', '<leader>gb', gs.blame_line, 'Blame line')
         map('n', '<leader>gd', gs.diffthis, 'Diff vs index (file)')
         map('n', '<leader>gD', function()
           gs.diffthis('~')
