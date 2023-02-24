2494a61f77a17c1f47cf28c19a7cdaa80b33bd44
Michael Uloth <hello@michaeluloth.com>
2023-02-24 15:53:45 -0500

    stop auto running ls after cd

--- config/zsh/mu/autocommands.zsh
+++
@@ -1,8 +0,0 @@
-# list directory contents after changing directories
-function chpwd() { 
-  # See: https://stackoverflow.com/questions/1371261/get-current-directory-or-folder-name-without-the-full-path
-  export CURRENT_DIRECTORY=${PWD##*/}
-
-  exa --all --group-directories-first 
-} 
-
