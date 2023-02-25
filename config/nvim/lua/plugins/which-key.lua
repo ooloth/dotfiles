3e34c909000c818c6cae3a852eb37ffdc8f92849
Michael Uloth <hello@michaeluloth.com>
2023-02-25 12:31:17 -0500

    create which-key "diagnostics" + "inspect" groups

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -1,5 +1,3 @@
-local Util = require('lazyvim.util')
-
 return {
   {
     'folke/which-key.nvim',
@@ -14,20 +12,17 @@ return {
         ['['] = { name = 'Prev' },
         ['<leader><tab>'] = { name = 'Tab' },
         ['<leader>b'] = { name = 'Buffer' },
-        ['<leader>c'] = { name = 'Code' },
+        ['<leader>d'] = { name = 'Diagnostics' },
         ['<leader>f'] = { name = 'Find' },
         ['<leader>g'] = { name = 'Git' },
         ['<leader>gt'] = { name = 'Toggle' },
+        ['<leader>i'] = { name = 'Inspect' },
         ['<leader>o'] = { name = 'Open' },
         ['<leader>q'] = { name = 'Quit/Session' },
         ['<leader>s'] = { name = 'Search' },
         ['<leader>u'] = { name = 'UI' },
         ['<leader>w'] = { name = 'Window' },
-        ['<leader>x'] = { name = 'Diagnostics' },
       }
-      if Util.has('noice.nvim') then
-        keymaps['<leader>n'] = { name = 'Notifications' }
-      end
       wk.register(keymaps)
     end,
   },
