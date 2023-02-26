34f405f5503aa8f3987146dd5f4559456ee9f1d3
Michael Uloth <hello@michaeluloth.com>
2023-02-25 22:21:34 -0500

    accept copilot suggestions with <cr>

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,10 @@
+vim.g.copilot_no_tab_map = true
+
+vim.cmd([[
+  imap <silent><script><expr> <cr> copilot#Accept("\<cr>")
+  " imap <silent><script><expr> <c-j> copilot#Accept("\<cr>")
+]])
+
 return {
   { 'github/copilot.vim' },
 }
