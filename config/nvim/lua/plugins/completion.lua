5da6bc27aed0fdc70420ca9b89f0913aadba0249
Michael Uloth <hello@michaeluloth.com>
2023-02-27 16:57:50 -0500

    add copilot suggests to nvim-cmp

---
+++ config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    event = 'VeryLazy',
+    config = function()
+      require('copilot').setup({
+        -- suggestion = { enabled = false },
+        -- panel = { enabled = false },
+      })
+    end,
+  },
+
+  {
+    'zbirenbaum/copilot-cmp',
+    after = { 'copilot.lua' },
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-emoji',
+      'zbirenbaum/copilot-cmp',
+    },
+    opts = function(_, opts)
+      local cmp = require('cmp')
+      opts.mapping = cmp.mapping.preset.insert({
+        ['<c-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<c-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<c-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<c-d>'] = cmp.mapping.scroll_docs(4),
+        -- ['<s-space>'] = cmp.mapping.complete(),
+        ['<cr>'] = cmp.mapping.abort(),
+        ['<tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      })
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = 'emoji' },
+        { name = 'copilot', group_index = 2 },
+      }))
+    end,
+  },
+}
