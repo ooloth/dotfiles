ca896063270b616a0b47e40cd3eeb8685f8e5371
Michael Uloth <hello@michaeluloth.com>
2023-02-28 13:49:32 -0500

    update alpha shortcut order by usefulness

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -15,12 +15,11 @@ return {
 
     dashboard.section.header.val = vim.split(logo, '\n')
     dashboard.section.buttons.val = {
-      dashboard.button('f', ' ' .. ' Find file', ':Telescope git_files<cr>'),
-      dashboard.button('n', ' ' .. ' New file', ':ene <BAR> startinsert<cr>'),
-      dashboard.button('r', ' ' .. ' Recent files', ':Telescope oldfiles cwd_only=true<cr>'),
-      dashboard.button('g', ' ' .. ' Find text', ':Telescope live_grep<cr>'),
-      dashboard.button('c', ' ' .. ' Config', ':e $MYVIMRC<cr>'),
       dashboard.button('s', '󰑓 ' .. ' Restore Session', [[:lua require("persistence").load() <cr>]]),
+      dashboard.button('r', ' ' .. ' Recent files', ':Telescope oldfiles cwd_only=true<cr>'),
+      dashboard.button('f', ' ' .. ' Find file', ':Telescope git_files<cr>'),
+      dashboard.button('g', ' ' .. ' Grep text', ':Telescope live_grep<cr>'),
+      dashboard.button('n', ' ' .. ' New file', ':ene <BAR> startinsert<cr>'),
       dashboard.button('l', '󰒲 ' .. ' Lazy', ':Lazy<cr>'),
       dashboard.button('q', ' ' .. ' Quit', ':qa<cr>'),
     }
