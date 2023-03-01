0607ee4745d5f481990f9dc1da755056ad7052ee
Michael Uloth <hello@michaeluloth.com>
2023-03-01 15:43:25 -0500

    remove unused bufferline close icons

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -7,6 +7,12 @@ return {
       { '<leader>p', '<cmd>BufferLineTogglePin<cr>', desc = 'Toggle pin' },
       { '<leader>P', '<cmd>BufferLineGroupClose ungrouped<cr>', desc = 'Close non-pinned buffers' },
     },
+    opts = {
+      options = {
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
   },
 
   -- only show the current tab's buffers in bufferline
