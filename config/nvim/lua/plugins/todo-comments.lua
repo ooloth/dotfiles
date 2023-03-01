33581b4be92c853d357de5ecd061ddae3a58873a
Michael Uloth <hello@michaeluloth.com>
2023-03-01 15:43:44 -0500

    remove ]t [t todo comment keymaps

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -3,20 +3,6 @@ return {
     'folke/todo-comments.nvim',
     keys = function() -- replace all keys
       return {
-        {
-          ']t',
-          function()
-            require('todo-comments').jump_next()
-          end,
-          desc = 'Next todo comment',
-        },
-        {
-          '[t',
-          function()
-            require('todo-comments').jump_prev()
-          end,
-          desc = 'Previous todo comment',
-        },
         -- "find"
         { '<leader>ft', '<cmd>TodoTelescope<cr>', desc = 'Todo' },
       }
