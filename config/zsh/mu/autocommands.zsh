64c43351c700e9cf55190f0e304c6536ff6c31e6
Michael Uloth <hello@michaeluloth.com>
2023-03-01 22:17:17 -0500

    only attempt to activate venvs on work laptop

--- config/zsh/mu/autocommands.zsh
+++ config/zsh/mu/autocommands.zsh
@@ -2,5 +2,8 @@ function chpwd() {
   # reset CURRENT_DIRECTORY env var after changing directories
   # See: https://stackoverflow.com/questions/1371261/get-current-directory-or-folder-name-without-the-full-path
   export CURRENT_DIRECTORY=${PWD##*/}
-  activate_venv
+
+  if $IS_WORK_LAPTOP; then
+    activate_venv
+  fi
 }
