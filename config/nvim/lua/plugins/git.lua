5dcc1190f2029d4db7299cd4a128f1ecd332703b
Michael Uloth <hello@michaeluloth.com>
2023-03-02 00:36:34 -0500

    wip: customize diffview keymaps

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -19,7 +19,21 @@ return {
       }
     end,
     -- see: https://github.com/sindrets/diffview.nvim#configuration
-    opts = {},
+    -- opts = {
+    -- opts = function(_, opts)
+    --   local actions = require('diffview.actions')
+    --   opts.keymaps = {
+    --     view = {
+    --       { 'n', '<j>', actions.select_next_entry, { desc = 'Open the diff for the next file' } },
+    --       { 'n', '<k>', actions.select_prev_entry, { desc = 'Open the diff for the previous file' } },
+    --     },
+    --     file_panel = {
+    --       { 'n', '<c-u>', actions.scroll_view(-0.25), { desc = 'Scroll the view up' } },
+    --       { 'n', '<c-d>', actions.scroll_view(0.25), { desc = 'Scroll the view down' } },
+    --     },
+    --   }
+    -- end,
+    -- },
   },
 
   {
