1221d67ccf52dfd794c9c5c0d90e8eb19f76b055
Michael Uloth <hello@michaeluloth.com>
2023-03-03 00:48:56 -0500

    replace vim-surround with mini.surround

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { 'catppuccin', enabled = false },
-  { 'echasnovski/mini.surround', enabled = false },
   { 'folke/tokyonight.nvim', enabled = false },
   { 'ggandor/leap.nvim', enabled = false },
   { 'ggandor/flit.nvim', enabled = false },
