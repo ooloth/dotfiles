6bd371c4ad57e535d83f17e2597a3c8661590317
Michael Uloth <hello@michaeluloth.com>
2023-03-03 00:55:24 -0500

    use lazyvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,2 @@
-local config = 'lazyvim' -- 'mine' or 'lazyvim'
-
-if config == 'lazyvim' then
-  require('config.lazy')
-else
-  require('mu.plugins-setup') -- should be first
-  -- require('mu.colorscheme')
-  require('mu.keymaps')
-  -- require('mu.options')
-  -- require('mu.plugins.autopairs')
-  -- require('mu.plugins.comment')
-  -- require('mu.plugins.gitsigns')
-  -- require('mu.plugins.lsp.lspsaga') -- set up before mason
-  require('mu.plugins.lsp.mason')
-  require('mu.plugins.lualine')
-  require('mu.plugins.nvim-cmp')
-  require('mu.plugins.nvim-tree')
-  -- require('mu.plugins.telescope')
-  require('mu.plugins.treesitter')
-  require('mu.plugins.trouble')
-  require('mu.plugins.vim-floaterm')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins (see https://github.com/LazyVim/starter)
+require('config.lazy')
