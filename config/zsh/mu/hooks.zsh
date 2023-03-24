7d65e4ada1b8edd5a496e8e1b2137cd13944671a
Michael Uloth <hello@michaeluloth.com>
2023-03-24 15:50:19 -0400

    rename autocommands.zsh -> hooks.zsh

--- config/zsh/mu/hooks.zsh
+++ config/zsh/mu/hooks.zsh
@@ -3,7 +3,6 @@ function chpwd() {
   # See: https://stackoverflow.com/questions/1371261/get-current-directory-or-folder-name-without-the-full-path
   export CURRENT_DIRECTORY=${PWD##*/}
 
-  if $IS_WORK_LAPTOP; then
-    activate_venv
-  fi
+  # automatically activate appropriate venv when cwd changes
+  activate_venv
 }
