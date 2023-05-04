d165f4fb6edc134cc47d750d1b915e3b8b9a7734
Michael Uloth <hello@michaeluloth.com>
2023-05-03 21:32:24 -0400

    start migrating to lazy's dap config

--- config/nvim/lua/plugins/debug.lua
+++ config/nvim/lua/plugins/debug.lua
@@ -2,15 +2,15 @@
 
 local mason_nvim_dap = require('plugins.dap.mason-nvim-dap')
 local dap = require('plugins.dap.dap')
--- local javascript = require('plugins.dap.javascript')
+local javascript = require('plugins.dap.javascript')
 -- local python = require('plugins.dap.python')
 -- local dap_ui = require('plugins.dap.dap-ui')
 
 return {
   -- 1. automatically install necessary debuggers (not vim-related)
-  mason_nvim_dap,
+  -- mason_nvim_dap,
   -- 2. install nvim-dap to allow connecting neovim to debuggers
-  dap,
+  -- dap,
   -- 3. install + configure debug adapters
   -- javascript,
   -- python,
