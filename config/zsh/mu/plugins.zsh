991e55342d715fdeb54d83b24d06038ed2b4b29b
Michael Uloth <hello@michaeluloth.com>
2023-05-05 17:08:38 -0400

    add copilot cli aliases

--- config/zsh/mu/plugins.zsh
+++ config/zsh/mu/plugins.zsh
@@ -1,5 +1,5 @@
 # fnm
-eval "$(fnm env --use-on-cd --log-level=error)" 
+eval "$(fnm env --use-on-cd --log-level=error)"
 
 # prompt
 # eval "$(starship init zsh)"
@@ -11,4 +11,7 @@ source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # zsh-syntax-highlighting
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# github-copilot-cli
+eval "$(github-copilot-cli alias -- "$0")"
 
