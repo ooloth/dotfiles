53294c04e0383f39833d586b54377ac20f7f19f8
Michael Uloth <hello@michaeluloth.com>
2023-06-15 14:17:09 -0400

    upgrade watchdog after reinstall python dependencies

--- config/zsh/mu/work.zsh
+++ config/zsh/mu/work.zsh
@@ -19,7 +19,7 @@ if $IS_WORK_LAPTOP; then
   psa() { cd $HOME/Repos/recursionpharma/phenoservice-api }
   psc() { cd $HOME/Repos/recursionpharma/phenoservice-consumer }
   r() { cd $HOME/Repos/recursionpharma }
-  rv() { pip install -U 'roadie[cli]' && roadie venv }
+  rv() { pip install -U 'roadie[cli]' && roadie venv && pip install --upgrade watchdog }
   rx3() { cd $HOME/Repos/recursionpharma/rxrx3-app }
 
   activate_venv() {
