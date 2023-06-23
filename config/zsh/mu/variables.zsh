78bbe208de7ccec2610e232831ea86494b4ea5e6
Michael Uloth <hello@michaeluloth.com>
2023-06-23 15:29:27 -0400

    sort vars alphabetically

--- config/zsh/mu/variables.zsh
+++ config/zsh/mu/variables.zsh
@@ -2,10 +2,6 @@ export EDITOR=code
 
 export GOKU_EDN_CONFIG_FILE=$HOME/.config/karabiner/karabiner.edn
 
-export IS_WORK_LAPTOP=$( [[ -d "$HOME/Repos/recursionpharma" ]] && echo "true" || echo "false" )
-
-export K9SCONFIG=$HOME/.config/k9s
-
 # TODO: customize HISTORY behavior (validate ideas below + use with fzf?)
 export HISTSIZE=10000
 SAVEHIST=10000
@@ -16,6 +12,10 @@ setopt HIST_IGNORE_SPACE
 setopt HIST_IGNORE_ALL_DUPS
 # alias history='history 0'
 # alias h='history | grep'
+
+export IS_WORK_LAPTOP=$( [[ -d "$HOME/Repos/recursionpharma" ]] && echo "true" || echo "false" )
+
+export K9SCONFIG=$HOME/.config/k9s
 
 export PATH=/usr/local/bin:$PATH # Add Homebrew's executable directory to front of PATH
 
