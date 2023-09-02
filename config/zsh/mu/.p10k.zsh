09c5ed3092b28596b0d96fd25f0b875361e7d7af
Michael Uloth <hello@michaeluloth.com>
2023-09-01 22:58:14 -0400

    new host name colors

--- config/zsh/mu/.p10k.zsh
+++ config/zsh/mu/.p10k.zsh
@@ -917,13 +917,13 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=4
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=5
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=0
   # Context color in SSH without privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=4
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=5
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_BACKGROUND=0
   # Default context color (no privileges, no SSH).
-  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=4
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=5
   typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=0
 
   # Context format when running with privileges: user@hostname.
