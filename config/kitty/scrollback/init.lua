d63885808732d73d30834e042d1fc132f7303518
Michael Uloth <hello@michaeluloth.com>
2023-12-17 16:30:27 -0500

    add mode to custom statusline

--- config/kitty/scrollback/init.lua
+++ config/kitty/scrollback/init.lua
@@ -1,12 +1,81 @@
+-- statusline
+local modes = {
+  ["n"] = "NORMAL",
+  ["no"] = "NORMAL",
+  ["nt"] = "NORMAL",
+  ["v"] = "VISUAL",
+  ["V"] = "VISUAL LINE",
+  [""] = "VISUAL BLOCK",
+  ["s"] = "SELECT",
+  ["S"] = "SELECT LINE",
+  [""] = "SELECT BLOCK",
+  ["i"] = "INSERT",
+  ["ic"] = "INSERT",
+  ["R"] = "REPLACE",
+  ["Rv"] = "VISUAL REPLACE",
+  ["c"] = "COMMAND",
+  ["cv"] = "VIM EX",
+  ["ce"] = "EX",
+  ["r"] = "PROMPT",
+  ["rm"] = "MOAR",
+  ["r?"] = "CONFIRM",
+  ["!"] = "SHELL",
+  ["t"] = "TERMINAL",
+}
+
+local function mode()
+  local current_mode = vim.api.nvim_get_mode().mode
+  return string.format(" %s ", modes[current_mode]):upper()
+end
+
+-- TODO: set the highlight groups reference here elsewhere (for each theme)
+local function update_mode_colors()
+  local current_mode = vim.api.nvim_get_mode().mode
+  local mode_color = "%#StatusLineAccent#"
+  if modes[current_mode] == "NORMAL" then
+    mode_color = "%#StatuslineAccent#"
+  elseif modes[current_mode] == "INSERT" then
+    mode_color = "%#StatuslineInsertAccent#"
+  elseif modes[current_mode]:find('VISUAL') then
+    mode_color = "%#StatuslineVisualAccent#"
+  elseif modes[current_mode] == "REPLACE" then
+    mode_color = "%#StatuslineReplaceAccent#"
+  elseif modes[current_mode] == "COMMAND" then
+    mode_color = "%#StatuslineCmdLineAccent#"
+  elseif modes[current_mode] == "TERMINAL" then
+    mode_color = "%#StatuslineTerminalAccent#"
+  end
+  return mode_color
+end
+
+Statusline = {}
+
+Statusline.update = function()
+  return table.concat {
+    "%#Statusline#",
+    update_mode_colors(),
+    mode(),
+    "%#Normal# ",
+    searchcount(),
+    -- filepath(),
+    -- filename(),
+    -- "%#Normal#",
+    -- lsp(),
+    -- "%=%#StatusLineExtra#",
+    -- filetype(),
+    -- lineinfo(),
+  }
+end
+
 local opt = vim.opt
 
 opt.clipboard = "unnamedplus" -- Sync with system clipboard
--- opt.cmdheight = 0
+opt.cmdheight = 0
 opt.completeopt = "menu,menuone,noselect"
 opt.cursorline = true          -- Enable highlighting of the current line
 opt.formatoptions = "jcroqlnt" -- tcqj
 opt.ignorecase = true          -- Ignore case
-opt.laststatus = 0
+opt.laststatus = 3             -- Always show one global statusline
 opt.mouse = "a"                -- Enable mouse mode
 opt.number = false             -- Print line number
 opt.pumblend = 10              -- Popup blend
@@ -14,15 +83,19 @@ opt.pumheight = 10             -- Maximum number of entries in a popup
 opt.relativenumber = false     -- Relative line numbers
 opt.scrolloff = 10             -- Lines of context
 opt.shortmess:append({ W = true, I = true, c = true })
--- opt.showmode = false       -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8              -- Columns of context
+opt.showmode = false           -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8          -- Columns of context
 opt.signcolumn = "no"
-opt.smartcase = true               -- Don't ignore case with capitals
-opt.termguicolors = true           -- True color support
+opt.smartcase = true           -- Don't ignore case with capitals
+-- opt.statusline = "%f %L"
+-- opt.statusline = "%n:%f%r%m |%{&ff}/%{&fenc}| |%l:%c/%LL| %{strftime('%H:%M')}"
+-- opt.statusline = '%{Statusline.update()}'
+opt.statusline = '%!v:lua.Statusline.update()' -- see :help statusline, :help v:lua
+opt.termguicolors = true                       -- True color support
 opt.timeoutlen = 300
-opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winminwidth = 5                -- Minimum window width
-opt.wrap = false                   -- Disable line wrap
+opt.wildmode = "longest:full,full"             -- Command-line completion mode
+opt.winminwidth = 5                            -- Minimum window width
+opt.wrap = false                               -- Disable line wrap
 
 -- Nicer line highlighting while using Material Ocean theme in terminal
 -- see: https://neovim.io/doc/user/syntax.html#highlight-groups
