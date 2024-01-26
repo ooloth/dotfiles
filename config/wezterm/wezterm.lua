f40b5620c515c773a6d55b3dfb9de525e1a7698a
Michael Uloth <hello@michaeluloth.com>
2024-01-26 14:48:29 -0500

    center wezterm content in window

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,15 +9,38 @@ config.color_scheme = 'Catppuccin Mocha' -- see: https://wezfurlong.org/wezterm/
 -- config.color_scheme = 'MaterialOcean' -- see: https://wezfurlong.org/wezterm/colorschemes/index.html
 config.enable_tab_bar = false
 config.font = wezterm.font('UbuntuMono Nerd Font') -- see: https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
--- config.font = wezterm.font('Ubuntu Mono') -- see: https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+-- config.font = wezterm.font_with_fallback({ 'UbuntuMono Nerd Font', 'Apple Color Emoji' }) -- see: https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
 config.font_size = 16
 config.line_height = 1.28
 config.window_decorations = 'RESIZE'
--- config.window_padding = {
---   left = 0,
---   right = 0,
---   top = 0,
---   bottom = 0,
--- }
+config.window_padding = {
+  top = 6,
+  right = 8,
+  bottom = 6,
+  left = 8,
+}
+
+-- see: https://github.com/wez/wezterm/issues/1124#issuecomment-1755513743
+local center_content = function(window, pane)
+  local win_dim = window:get_dimensions()
+  local tab_dim = pane:tab():get_size()
+  local overrides = window:get_config_overrides() or {}
+  local padding_left = (win_dim.pixel_width - tab_dim.pixel_width) / 2
+  local padding_top = (win_dim.pixel_height - tab_dim.pixel_height) / 2
+  local new_padding = {
+    left = padding_left,
+    right = 0,
+    top = padding_top,
+    bottom = 0,
+  }
+  if overrides.window_padding and new_padding.left == overrides.window_padding.left then
+    return
+  end
+  overrides.window_padding = new_padding
+  window:set_config_overrides(overrides)
+end
+
+wezterm.on('window-resized', center_content)
+wezterm.on('window-config-reloaded', center_content)
 
 return config
