a26e72dc5a0eb3d71f7e1231f8e04b856651563d
Michael Uloth <hello@michaeluloth.com>
2024-01-26 14:46:43 -0500

    back to powerlevel10k prompt

--- config/zsh/mu/plugins.zsh
+++ config/zsh/mu/plugins.zsh
@@ -2,9 +2,9 @@
 eval "$(fnm env --use-on-cd --log-level=error)"
 
 # prompt
-eval "$(starship init zsh)"
-# source $(brew --prefix)/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme
-# source $HOME/.config/zsh/mu/.p10k.zsh # to customize, run `p10k configure` or edit config/zsh/mu/.p10k.zsh
+# eval "$(starship init zsh)"
+source $(brew --prefix)/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme
+source $HOME/.config/zsh/mu/.p10k.zsh # to customize, run `p10k configure` or edit config/zsh/mu/.p10k.zsh
 
 # zsh-autosuggestions
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
