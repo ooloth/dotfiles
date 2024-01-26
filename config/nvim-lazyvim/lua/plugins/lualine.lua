b233af400d8e69c342cd54615758a70f838fac2f
Michael Uloth <hello@michaeluloth.com>
2024-01-26 16:00:28 -0500

    simplify lualine

--- config/nvim-lazyvim/lua/plugins/lualine.lua
+++ config/nvim-lazyvim/lua/plugins/lualine.lua
@@ -1,3 +1,6 @@
+-- see: https://www.lazyvim.org/plugins/ui#lualinenvim
+
+local Util = require('lazyvim.util')
 local icons = require('lazyvim.config').icons
 
 local function get_venv()
@@ -6,7 +9,7 @@ end
 
 local options = {
   component_separators = '',
-  -- disabled_filetypes = {},
+  disabled_filetypes = { tabline = { 'alpha', 'dashboard', 'starter' } },
   globalstatus = true,
   section_separators = '',
   theme = 'auto',
@@ -17,7 +20,6 @@ local sections = {
   lualine_b = {
     {
       'diagnostics',
-      padding = { left = 1, right = 0 },
       symbols = {
         error = icons.diagnostics.Error,
         warn = icons.diagnostics.Warn,
@@ -27,31 +29,26 @@ local sections = {
     },
   },
   lualine_c = {
+    { Util.lualine.pretty_path() },
     -- the larger the shorting_target number, the sooner the file path abbreviates
     -- see: https://github.com/nvim-lualine/lualine.nvim#filename-component-options
-    { 'filename', path = 1, shorting_target = 60, symbols = { modified = '', readonly = '', unnamed = '' } },
+    -- { 'filename', path = 1, shorting_target = 60, symbols = { modified = '', readonly = '', unnamed = '' } },
   },
   lualine_x = { 'filetype', get_venv },
-  lualine_y = { 'progress' },
-  lualine_z = { 'location' },
-}
-
-local inactive_sections = {
-  -- see: https://github.com/nvim-lualine/lualine.nvim#filename-component-options
-  lualine_c = { { 'filename', path = 1, shorting_target = 100 } },
+  lualine_y = { 'diff' },
+  lualine_z = { 'branch' },
 }
 
 return {
   {
     'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
     opts = function()
       return {
         extensions = { 'neo-tree', 'nvim-dap-ui', 'quickfix', 'toggleterm' },
         inactive_sections = {},
-        -- inactive_sections = inactive_sections,
         options = options,
         sections = {},
-        -- sections = sections,
         tabline = sections,
       }
     end,
