84df26effc4795e7e917c32c1f09776e72c8962f
Michael Uloth <hello@michaeluloth.com>
2024-01-27 23:51:47 -0500

    move the cache v compute decision into the helper function

--- config/nvim-lazyvim/lua/plugins/formatting.lua
+++ config/nvim-lazyvim/lua/plugins/formatting.lua
@@ -1,21 +1,24 @@
 -- see: https://github.com/stevearc/conform.nvim/blob/master/lua/conform/formatters/prettier.lua
 -- see: https://www.lazyvim.org/extras/formatting/prettier
 
-local mason_registry = require('mason-registry')
-
--- cache the result of prefer_bin_from_node_modules function after the first call
+-- cache the result of the first prefer_bin_from_node_modules call
 local prettier_command = ''
 
 local function prefer_bin_from_node_modules(executable_name)
-  -- get the binary from node_modules if it exists
+  -- use the cached result if it exists
+  if prettier_command ~= '' then
+    return prettier_command
+  end
+
+  -- otherwise, get the path to the node_modules binary (if it exists)
   local node_modules_path = vim.fn.getcwd() .. '/node_modules/.bin/' .. executable_name
   if vim.fn.executable(node_modules_path) == 1 then
     prettier_command = node_modules_path
     return node_modules_path
   end
 
-  -- otherwise, get it from Mason
-  local mason_path = mason_registry.get_package(executable_name):get_install_path()
+  -- otherwise, get the path to the Mason binary
+  local mason_path = require('mason-registry').get_package(executable_name):get_install_path()
     .. '/node_modules/.bin/'
     .. executable_name
   prettier_command = mason_path
@@ -47,21 +50,11 @@ return {
         -- see: https://prettier.io/docs/en/plugins.html#community-plugins
         prettier = function()
           return {
-            command = prettier_command ~= '' and prettier_command or prefer_bin_from_node_modules('prettier'),
+            command = prefer_bin_from_node_modules('prettier'),
             require_cwd = true, -- only activate if a config file is found
           }
         end,
       },
     },
   },
-
-  -- {
-  --   'jay-babu/mason-null-ls.nvim',
-  --   -- see: https://github.com/jay-babu/mason-null-ls.nvim#primary-source-of-truth-is-null-ls
-  --   opts = {
-  --     ensure_installed = nil, -- defined in null-ls sources above
-  --     automatic_installation = true,
-  --     automatic_setup = true,
-  --   },
-  -- },
 }
