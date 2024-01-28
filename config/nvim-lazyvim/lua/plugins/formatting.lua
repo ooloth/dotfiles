736c3636cd30539601ad413a1c5b4967643f0e49
Michael Uloth <hello@michaeluloth.com>
2024-01-27 23:27:28 -0500

    cache the prettier command the first time its computed

--- config/nvim-lazyvim/lua/plugins/formatting.lua
+++ config/nvim-lazyvim/lua/plugins/formatting.lua
@@ -3,33 +3,43 @@
 
 local mason_registry = require('mason-registry')
 
--- see: https://github.com/fredrikaverpil/dotfiles/blob/main/nvim-lazyvim/lua/plugins/lsp.lua
-local function prefer_node_modules(executable_name)
-  -- Return the path to the executable if $VIRTUAL_ENV is set and the binary exists somewhere beneath the $VIRTUAL_ENV path, otherwise get it from Mason
+-- cache the result of prefer_bin_from_node_modules function after the first call
+local prettier_command = ''
+
+local function prefer_bin_from_node_modules(executable_name)
+  -- get the binary from node_modules if it exists
   local node_modules_path = vim.fn.getcwd() .. '/node_modules/.bin/' .. executable_name
   if vim.fn.executable(node_modules_path) == 1 then
-    vim.api.nvim_echo({ { 'Using path for ' .. executable_name .. ': ' .. node_modules_path, 'None' } }, false, {})
+    prettier_command = node_modules_path
     return node_modules_path
   end
 
+  -- otherwise, get it from Mason
   local mason_path = mason_registry.get_package(executable_name):get_install_path()
     .. '/node_modules/.bin/'
     .. executable_name
-  vim.api.nvim_echo({ { 'Using path for ' .. executable_name .. ': ' .. mason_path, 'None' } }, false, {})
+  prettier_command = mason_path
   return mason_path
 end
 
 return {
   {
-    'williamboman/mason.nvim',
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, 'prettier')
-    end,
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    },
+    opts = {
+      ensure_installed = { 'prettier' },
+    },
   },
 
   {
     -- see: https://github.com/stevearc/conform.nvim
     'stevearc/conform.nvim',
+    dependencies = {
+      'williamboman/mason.nvim',
+    },
     event = 'VeryLazy',
     opts = {
       formatters = {
@@ -37,7 +47,7 @@ return {
         -- see: https://prettier.io/docs/en/plugins.html#community-plugins
         prettier = function()
           return {
-            command = prefer_node_modules('prettier'),
+            command = prettier_command ~= '' and prettier_command or prefer_bin_from_node_modules('prettier'),
             require_cwd = true, -- only activate if a config file is found
           }
         end,
