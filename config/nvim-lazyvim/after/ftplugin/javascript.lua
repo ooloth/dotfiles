d9e04d13a05c57d1b0e9347176979954c5ac8e49
Michael Uloth <hello@michaeluloth.com>
2024-01-28 22:49:20 -0500

    use tsserver as lsp for all js-based files

--- config/nvim-lazyvim/after/ftplugin/javascript.lua
+++ config/nvim-lazyvim/after/ftplugin/javascript.lua
@@ -1,8 +1,18 @@
--- formatting (see: https://github.com/stevearc/conform.nvim#setup)
-require('conform').setup({
-  formatters_by_ft = {
-    javascript = { 'prettier' },
-    javascriptreact = { 'prettier' },
+-- see: https://www.lazyvim.org/extras/lang/typescript
+
+-- install everything we need (see: https://mason-registry.dev/registry/list)
+require('mason-tool-installer').setup({ ensure_installed = { 'eslint-lsp', 'prettier', 'tsserver' } })
+vim.api.nvim_command('MasonToolsInstall')
+
+-- lsp (see: https://github.com/neovim/nvim-lspconfig#quickstart)
+require('lspconfig').tsserver.setup({
+  -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
+  -- see: https://github.com/typescript-language-server/typescript-language-server/blob/master/docs/configuration.md#tsserver-options
+  settings = {
+    -- TODO: prefer node modules version over mason version
+    completions = {
+      completeFunctionCalls = true,
+    },
   },
 })
 
