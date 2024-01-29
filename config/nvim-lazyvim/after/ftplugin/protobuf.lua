bf442bc16fa8a9f5072d6b3ec64d236d72a32671
Michael Uloth <hello@michaeluloth.com>
2024-01-28 22:45:55 -0500

    lint protobuf with buf_lint and format with buf

--- config/nvim-lazyvim/after/ftplugin/protobuf.lua
+++ config/nvim-lazyvim/after/ftplugin/protobuf.lua
@@ -1,14 +1,12 @@
--- install the formatter
+-- install the formatter (see: https://mason-registry.dev/registry/list)
 -- require('mason-tool-installer').setup({ ensure_installed = { 'buf' } })
 -- vim.api.nvim_command('MasonToolsInstall')
 
--- formatting (see: https://github.com/stevearc/conform.nvim#setup)
-require('conform').setup({
-  formatters_by_ft = {
-    protobuf = { 'buf' },
-  },
-})
+-- linting (see: https://github.com/mfussenegger/nvim-lint#usage)
+require('lint').linters_by_ft.protobuf = { 'buf_lint' }
 
---  TODO: lsp
+-- formatting (see: https://github.com/stevearc/conform.nvim#options)
+require('conform').formatters_by_ft.protobuf = { 'buf' }
+
+--  TODO: lsp (not yet; see: https://github.com/bufbuild/buf/pull/2662)
 --  TODO: treesitter
---  TODO: linting
