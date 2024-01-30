d5ba4f7b28c1f83addda3623e23ba9389444122b
Michael Uloth <hello@michaeluloth.com>
2024-01-29 23:43:17 -0500

    add astro lsp support

--- config/nvim-lazyvim/after/ftplugin/astro.lua
+++ config/nvim-lazyvim/after/ftplugin/astro.lua
@@ -1,3 +1,10 @@
+-- install everything we need (see: https://mason-registry.dev/registry/list)
+require('mason-tool-installer').setup({ ensure_installed = { 'astro' } })
+vim.api.nvim_command('MasonToolsInstall')
+
+-- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#astro
+require('lspconfig').astro.setup({})
+
 -- formatting (see: https://github.com/stevearc/conform.nvim#setup)
 require('conform').setup({
   formatters_by_ft = {
@@ -7,11 +14,10 @@ require('conform').setup({
   },
 })
 
---  TODO: lsp
 --  TODO: treesitter
 --  TODO: linting
 --  TODO: dap?
-
+--
 -- TODO: https://github.com/wuelnerdotexe/vim-astro
 -- TODO: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#astro
 -- TODO: https://github.com/virchau13/tree-sitter-astro
