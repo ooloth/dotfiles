0a7decbae5fd109f6cdc734ca8e2618613c8c2b9
Michael Uloth <hello@michaeluloth.com>
2024-02-02 00:07:13 -0500

    disable flash plugin

--- config/nvim-lazyvim/lua/plugins/core/disabled.lua
+++ config/nvim-lazyvim/lua/plugins/core/disabled.lua
@@ -1,6 +1,7 @@
 return {
   { 'akinsho/bufferline.nvim', enabled = false },
-  { 'echasnovski/mini.indentscope', enabled = false },
+  -- { 'echasnovski/mini.indentscope', enabled = false },
+  { 'folke/flash.nvim', enabled = false },
   { 'folke/tokyonight.nvim', enabled = false },
   { 'ggandor/leap.nvim', enabled = false },
   { 'ggandor/flit.nvim', enabled = false },
