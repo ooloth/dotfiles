82a70ac5a18a2e5356a6c841053de67082dbb951
Michael Uloth <hello@michaeluloth.com>
2024-02-02 21:55:43 -0500

    ignore blank lines when commenting paragraphs

---
+++ config/nvim-lazyvim/lua/plugins/editing/mini-comment.lua
@@ -0,0 +1,9 @@
+return {
+  'echasnovski/mini.comment',
+  opts = {
+    options = {
+      -- see: https://github.com/echasnovski/mini.comment?tab=readme-ov-file#default-config
+      ignore_blank_line = true,
+    },
+  },
+}
