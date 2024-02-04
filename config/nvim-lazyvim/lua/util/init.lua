aef42730fb1dcc2d8fbcf4168b88cdaa4fd48358
Michael Uloth <hello@michaeluloth.com>
2024-02-04 00:29:57 -0500

    add prefer_node_modules placeholder

--- config/nvim-lazyvim/lua/util/init.lua
+++ config/nvim-lazyvim/lua/util/init.lua
@@ -5,6 +5,7 @@ M.reset = function()
   return {}
 end
 
+-- For extending a list-type table with another list-type table only (not a dictionary).
 -- @type fun(list:table, values:table): table
 M.extend = function(list, values)
   vim.list_extend(list or {}, values)
