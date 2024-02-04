ea0d170430e107fd67d1f7adbff23bff2f120ccb
Michael Uloth <hello@michaeluloth.com>
2024-02-04 00:28:28 -0500

    add python lsp, formatting and wip linting support

--- config/nvim-lazyvim/lua/plugins/lang/python.lua
+++ config/nvim-lazyvim/lua/plugins/lang/python.lua
@@ -1,100 +1,147 @@
-return {}
-
--- -- -- -- -- -- install the formatters
--- -- -- -- -- require('mason-tool-installer').setup({ ensure_installed = { 'black', 'isort', 'pyright', 'ruff_format' } })
--- -- -- -- -- vim.api.nvim_command('MasonToolsInstall')
--- -- -- -- --
--- -- -- -- -- lsp (see: https://github.com/neovim/nvim-lspconfig#quickstart)
--- -- -- -- require('lspconfig').pyright.setup({
--- -- -- --   -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pyright
--- -- -- --   settings = {
--- -- -- --     -- TODO: prefer node modules version over mason version
--- -- -- --     pyright = {
--- -- -- --       settings = {
--- -- -- --         -- see: https://microsoft.github.io/pyright/#/settings
--- -- -- --         python = {
--- -- -- --           analysis = {
--- -- -- --             diagnosticMode = 'workspace',
--- -- -- --             typeCheckingMode = 'off', -- use pyright for lsp but mypy for type-checking
--- -- -- --             useLibraryCodeForTypes = true,
--- -- -- --           },
--- -- -- --           disableOrganizeImports = true, -- use ruff or isort for import sorting
--- -- -- --         },
--- -- -- --       },
--- -- -- --     },
--- -- -- --   },
--- -- -- -- })
--- -- -- --
--- -- -- -- cache the result of the first prefer_bin_from_venv call for each executable
--- -- -- local cached_commands = {
--- -- --   black = '',
--- -- --   isort = '',
--- -- --   ruff_format = '',
--- -- -- }
--- -- --
--- -- -- see: https://github.com/fredrikaverpil/dotfiles/blob/main/nvim-lazyvim/lua/plugins/lsp.lua
--- -- local function prefer_bin_from_venv(executable_name)
--- --   -- use the cached result if it exists
--- --   if cached_commands[executable_name] ~= '' then
--- --     return cached_commands[executable_name]
--- --   end
--- --
---   -- otherwise, get the path to the node_modules binary (if it exists)
---   if vim.env.VIRTUAL_ENV then
---     local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. '/**/bin/' .. executable_name, true, true)
---     local executable_path = table.concat(paths, ', ')
---     if executable_path ~= '' then
---       -- vim.api.nvim_echo({ { 'Using path for ' .. executable_name .. ': ' .. executable_path, 'None' } }, false, {})
---       cached_commands[executable_name] = executable_path
---       return executable_path
---     end
---   end
---
---   -- otherwise, get the path to the Mason binary
---   local mason_registry = require('mason-registry')
---   local mason_path = mason_registry.get_package(executable_name):get_install_path() .. '/venv/bin/' .. executable_name
---   -- vim.api.nvim_echo({ { 'Using path for ' .. executable_name .. ': ' .. mason_path, 'None' } }, false, {})
---   cached_commands[executable_name] = mason_path
---   return mason_path
--- end
---
 --  TODO: linting
+--  TODO: types
+--  TODO: testing
+--  TODO: dap
 
--- -- formatting (see: https://github.com/stevearc/conform.nvim#setup)
--- require('conform')
---   .setup({
---     formatters = {
---       black = function()
---         -- see: https://github.com/stevearc/conform.nvim/blob/master/lua/conform/formatters/black.lua
---         return {
---           command = prefer_bin_from_venv('black'),
---           condition = function(ctx)
---             return vim.fs.basename(ctx.filename) ~= 'README.md'
---           end,
---         }
---       end,
---       isort = function()
---         return {
---           command = prefer_bin_from_venv('isort'),
---         }
---       end,
---       ruff_format = function()
---         return {
---           command = prefer_bin_from_venv('ruff'),
---         }
---       end,
---     },
---     formatters_by_ft = {
---       --  TODO: choose formatter based on packages installed in virtualenv
---       python = { 'isort', 'black', 'ruff_format' },
---     },
---   })
---   (
---   --  TODO: lsp
---   --  TODO: treesitter
---   --  TODO: linting
---   --  TODO: dap
---
+-- see: https://www.lazyvim.org/extras/lang/python#nvim-lspconfig
+
+local extend = require('util').extend
+local is_installed_in_venv = require('util.prefer_venv').is_installed_in_venv
+local prefer_venv_executable = require('util.prefer_venv').prefer_venv_executable
+
+return {
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      extend(opts.ensure_installed, { 'black', 'flake8', 'isort', 'mypy', 'pyright', 'ruff', 'ruff-lsp', 'yapf' })
+    end,
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pyright
+        pyright = {
+          settings = {
+            -- see: https://microsoft.github.io/pyright/#/settings
+            python = {
+              analysis = {
+                diagnosticMode = 'workspace',
+                typeCheckingMode = 'off', -- use pyright for lsp but mypy for type-checking
+                useLibraryCodeForTypes = true,
+              },
+              disableOrganizeImports = true, -- use ruff or isort for import sorting
+            },
+          },
+        },
+        -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+        ruff_lsp = {},
+      },
+      setup = {
+        ruff_lsp = function()
+          require('lazyvim.util').lsp.on_attach(function(client, _)
+            if client.name == 'ruff_lsp' then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      extend(opts.ensure_installed, { 'python' })
+    end,
+  },
+
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = { python = { 'isort', 'black', 'ruff_format', 'yapf' } },
+      formatters = {
+        black = function()
+          -- see: https://github.com/stevearc/conform.nvim/blob/master/lua/conform/formatters/black.lua
+          return {
+            command = prefer_venv_executable('black'),
+            condition = function()
+              return is_installed_in_venv('black')
+            end,
+          }
+        end,
+        isort = function()
+          return {
+            command = prefer_venv_executable('isort'),
+            condition = function()
+              return is_installed_in_venv('isort')
+            end,
+          }
+        end,
+        ruff_format = function()
+          return {
+            command = prefer_venv_executable('ruff'),
+            condition = function()
+              return is_installed_in_venv('ruff')
+            end,
+          }
+        end,
+        yapf = function()
+          return {
+            command = prefer_venv_executable('yapf'),
+            condition = function()
+              return is_installed_in_venv('yapf')
+            end,
+          }
+        end,
+      },
+    },
+  },
+
+  {
+    'mfussenegger/nvim-lint',
+    -- see: https://www.lazyvim.org/plugins/linting#nvim-lint
+    opts = {
+      linters_by_ft = {
+        python = {
+          -- 'flake8',
+          'mypy',
+          'ruff_lint',
+        },
+      },
+      linters = {
+        -- TODO: make condition work for uninstalled linters (it currently includes them)
+        -- flake8 = function()
+        --   return {
+        --     cmd = prefer_venv_executable('flake8'),
+        --     condition = function()
+        --       return is_installed_in_venv('flake8')
+        --     end,
+        --   }
+        -- end,
+        mypy = function()
+          return {
+            cmd = prefer_venv_executable('mypy'),
+            condition = function()
+              return is_installed_in_venv('mypy')
+            end,
+          }
+        end,
+        ruff_lint = function()
+          return {
+            cmd = prefer_venv_executable('ruff'),
+            condition = function()
+              return is_installed_in_venv('ruff')
+            end,
+          }
+        end,
+      },
+    },
+  },
+}
+
 -- {lua require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')}
 -- {
 --     'mfussenegger/nvim-dap-python',
@@ -118,5 +165,3 @@ return {}
 --       -- }
 --     end,
 --   })
-
--- TODO: refactor to multiple after/ftplugin/python/*.lua files if this one gets too long?
