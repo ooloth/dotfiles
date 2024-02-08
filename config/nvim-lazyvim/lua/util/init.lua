2d30c0daed8aea211017d9bfb38ee8228ef83b93
Michael Uloth <hello@michaeluloth.com>
2024-02-08 17:35:40 -0500

    fix type error

--- config/nvim-lazyvim/lua/util/init.lua
+++ config/nvim-lazyvim/lua/util/init.lua
@@ -6,8 +6,9 @@ M.reset = function()
 end
 
 -- see: https://github.com/rcarriga/nvim-notify?tab=readme-ov-file#usage
+-- @type fun(): nil
 M.inspect = function(value)
-  vim.notify(vim.inspect(value), 'info')
+  vim.notify(vim.inspect(value), vim.log.levels.INFO)
 end
 
 -- For extending a list-type table with another list-type table only (not a dictionary).
