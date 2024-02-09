c82327d40af7e60b566e0768719ff642f0ee26bb
Michael Uloth <hello@michaeluloth.com>
2024-02-08 21:46:26 -0500

    fix PYTHONPATH so dap can find local project modules

--- config/nvim-lazyvim/lua/plugins/lang/python.lua
+++ config/nvim-lazyvim/lua/plugins/lang/python.lua
@@ -3,16 +3,19 @@ local inspect = require('util').inspect
 local is_installed_in_venv = require('util.prefer_venv').is_installed_in_venv
 local prefer_venv_executable = require('util.prefer_venv').prefer_venv_executable
 
--- get the python executable from the project venv (if active) for dap and neotest
-local python = prefer_venv_executable('python')
-vim.env.PYTHONPATH = '.'
--- vim.env.PYTHONPATH = python
+-- Help debugger find current project's local (not pip) modules:
+-- https://stackoverflow.com/a/63271966/8802485
+vim.env.PYTHONPATH = vim.fn.getcwd()
 
+-- TODO: automate creation of the pynvim venv and the installation of pynvim, debugpy + CopilotChat's pip deps during mac setup
 -- get python executable where pynvim is installed for running remote plugins written in python (see :h provider-python)
 -- see: https://github.com/neovim/pynvim/issues/498
 -- see: https://github.com/neovim/pynvim/issues/16#issuecomment-152417012
 local pynvim_python = vim.env.HOME .. '/.pyenv/versions/pynvim/bin/python'
 vim.g.python3_host_prog = pynvim_python
+
+-- get the python executable from the project venv (if active) for dap and neotest
+local python = prefer_venv_executable('python')
 
 -- see: https://github.com/stevearc/conform.nvim/blob/master/lua/conform/formatters/black.lua
 local get_formatter_options = function(formatter)
@@ -56,6 +59,7 @@ return {
         -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pyright
         pyright = {
           capabilities = (function()
+            -- Disable Pyright diagnostics (use flake8 or ruff for linting):
             -- see: https://www.reddit.com/r/neovim/comments/11k5but/how_to_disable_pyright_diagnostics/
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
@@ -83,7 +87,7 @@ return {
         ruff_lsp = function()
           require('lazyvim.util').lsp.on_attach(function(client, _)
             if client.name == 'ruff_lsp' then
-              -- Disable hover in favor of Pyright
+              -- Disable Ruff's hover in favor of Pyright's
               client.server_capabilities.hoverProvider = false
             end
           end)
@@ -139,33 +143,38 @@ return {
         { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
       },
       config = function()
-        local path = require('mason-registry').get_package('debugpy'):get_install_path()
-        require('dap-python').setup(path .. '/venv/bin/python')
+        -- use the debugpy installed in the pynvim venv so I don't have to install it in every project's venv:
+        local pynvim_debugpy_python = vim.env.HOME .. '/.pyenv/versions/pynvim/bin/debugpy' .. '/venv/bin/python'
 
-        -- local pynvim_debugpy_python = vim.env.HOME .. '/.pyenv/versions/pynvim/bin/debugpy' .. '/venv/bin/python'
-        -- .pyenv/versions/pynvim/lib/python3.12/site-packages
+        require('dap-python').setup(pynvim_debugpy_python, { include_configs = false, pythonPath = python })
 
-        -- require('dap-python').setup(pynvim_debugpy_python)
-        -- require('dap-python').setup(python, { include_configs = false, pythonPath = python })
-        -- require('dap-python').setup('python', { include_configs = false, pythonPath = python })
-        -- require('dap-python').setup(pynvim_python, { include_configs = false, pythonPath = pynvim_python })
+        -- After setting up the dap-python plugin, define default Python configurations (prefer project launch.json though):
+        require('dap').configurations.python = {
+          -- {
+          --   name = 'Python: MY Launch file',
+          --   type = 'python',
+          --   request = 'launch',
+          --   program = '${file}',
+          --   console = 'integratedTerminal',
+          --   pythonPath = python,
+          --   -- env = {
+          --   --   -- Solves issue where the debugger can't find the project's local modules:
+          --   --   -- https://stackoverflow.com/a/63271966/8802485
+          --   --   PYTHONPATH = vim.fn.getcwd(),
+          --   -- },
+          -- },
+          -- {
+          --   name = 'Python: Launch Flask server',
+          --   type = 'python',
+          --   request = 'launch',
+          --   module = 'flask',
+          --   console = 'integratedTerminal',
+          --   pythonPath = python,
+          --   args = { '--app', 'app', 'run' },
+          -- },
+        }
       end,
     },
-    opts = function(_, opts)
-      opts.configurations = opts.configurations or {}
-
-      table.insert(opts.configurations.python or {}, {
-        name = 'Launch Flask server',
-        type = 'python',
-        request = 'launch',
-        console = 'integratedTerminal',
-        module = 'flask',
-        pythonPath = function()
-          return python
-        end,
-        args = { '--app', 'app', 'run' },
-      })
-    end,
   },
 
   {
