0bf211b4cbb27274f5a32e72cf78305d8f717aef
Michael Uloth <hello@michaeluloth.com>
2024-02-08 21:54:56 -0500

    leader-t* to run tests

--- config/nvim-lazyvim/lua/plugins/testing/neotest.lua
+++ config/nvim-lazyvim/lua/plugins/testing/neotest.lua
@@ -5,13 +5,14 @@ return {
   },
   -- stylua: ignore
   keys = {
-    { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
-    { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
-    { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
-    { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
-    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
-    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
-    { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
+    { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run last" },
+    { "<leader>tn", function() require("neotest").run.run() end, desc = "Run nearest" },
+    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show output" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle summary" },
     { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
+    { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run file" },
+    { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run all test files" },
+    { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Watch file (toggle)" },
+    { "<leader>tW", function() require("neotest").watch.toggle(vim.loop.cwd()) end, desc = "Watch all test files (toggle)" },
   },
 }
