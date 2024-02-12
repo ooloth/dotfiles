e4a6669f4b07ded510c121e19c9d4b5c13dd80c0
Michael Uloth <hello@michaeluloth.com>
2024-02-12 14:44:12 -0500

    fix(hooks): don't output gcloud project while reading it

--- config/zsh/mu/hooks.zsh
+++ config/zsh/mu/hooks.zsh
@@ -43,8 +43,17 @@ function set_gcloud_project_env_var() {
   # if the gcloud command isn't found, do nothing
   command -v gcloud &> /dev/null || return
 
+  # if GOOGLE_CLOUD_PROJECT is already set, do nothing
+  [[ -n "$GOOGLE_CLOUD_PROJECT" ]] && [[ "$GOOGLE_CLOUD_PROJECT" != "(unset)" ]] && return
+
+  # get current project (and don't output the result to the terminal)
+  current_project=$(gcloud config get-value project 2> /dev/null)
+
+  # if current_project is "(unset)", do nothing
+  [[ "$current_project" == "(unset)" ]] && return
+
   # otherwise, set GOOGLE_CLOUD_PROJECT to the current gcloud project (for dap)
-  export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
+  export GOOGLE_CLOUD_PROJECT=$current_project
 }
 
 # automatically activate appropriate venv when zsh first loads (called again in chpwd hook whenever cwd changes)
