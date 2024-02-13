998d4dd7a58b052bff009fb8c70c2e77a3642dea
Michael Uloth <hello@michaeluloth.com>
2024-02-13 17:11:16 -0500

    feat(telescope): open one or many selected files with same <CR> keypress

--- config/nvim-lazyvim/lua/plugins/searching/telescope.lua
+++ config/nvim-lazyvim/lua/plugins/searching/telescope.lua
@@ -2,6 +2,23 @@ local actions = require('telescope.actions')
 -- local undo_actions = require('telescope-undo.actions')
 local Util = require('lazyvim.util')
 
+-- Support opening multiple files in the same picker session:
+-- see: https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 return {
   {
     'nvim-telescope/telescope.nvim',
@@ -66,6 +83,8 @@ return {
         },
         mappings = {
           i = {
+            -- optionally select/deselect multiple files with <Tab>/<S-Tab>, then open all with <CR>
+            ['<CR>'] = select_one_or_multi, -- still opens just one file if no multi-selection
             ['<esc>'] = actions.close,
             ['<C-j>'] = actions.move_selection_next,
             ['<C-k>'] = actions.move_selection_previous,
