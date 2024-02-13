0cca3dbddf13635b048932fd8c6e56413db9d2de
Michael Uloth <hello@michaeluloth.com>
2024-02-13 14:25:33 -0500

    fix(neotest): add treesitter as dependency

--- config/nvim-lazyvim/lua/plugins/testing/neotest.lua
+++ config/nvim-lazyvim/lua/plugins/testing/neotest.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-neotest/neotest',
   dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     { 'folke/neodev.nvim', opts = { library = { plugins = { 'neotest' }, types = true } } },
   },
   -- stylua: ignore
