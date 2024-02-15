4692589de12739af82a5dfbd287f53d9ef3f23f4
Michael Uloth <hello@michaeluloth.com>
2024-02-14 21:41:28 -0500

    leader-a* to chat with copilot

--- config/nvim-lazyvim/lua/plugins/ai/copilotchat.lua
+++ config/nvim-lazyvim/lua/plugins/ai/copilotchat.lua
@@ -1,13 +1,39 @@
+-- see: https://github.com/jellydn/lazy-nvim-ide/blob/main/lua/plugins/extras/copilot-chat.lua
+---Get all the changes in the git repository
+---@param staged? boolean
+---@return string
+local function get_git_diff(staged)
+  local cmd = staged and 'git diff --staged' or 'git diff'
+  local handle = io.popen(cmd)
+  if not handle then
+    return ''
+  end
+
+  local result = handle:read('*a')
+  handle:close()
+  return result
+end
+
 -- NOTE: this is a remote plugin written in Python, so it requires the `pynvim` package to be installed
 -- I've created a "pynvim" venv for neovim and installed pynvim and CopilotChat.nvim's pip dependencies there
 -- see: https://github.com/neovim/pynvim/issues/498#issuecomment-1114461723
 -- see: https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#installation
 
--- NOTE: solve "ambiguous use of user-defined command" error by restarting nvim + :UpdateRemotePlugins
+-- NOTE: can solve "ambiguous use of user-defined command" error by restarting nvim + :UpdateRemotePlugins
 -- see: https://github.com/jellydn/CopilotChat.nvim/issues/43#issuecomment-1923807768
 return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      {
+        'folke/which-key.nvim',
+        opts = {
+          defaults = {
+            ['<leader>a'] = { name = '+AI' },
+          },
+        },
+      },
+    },
     opts = {
       debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
       disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
@@ -31,47 +57,36 @@ return {
       vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
     end,
     event = 'VeryLazy',
+    -- stylua: ignore
     keys = {
-      -- Code related commands
-      { '<leader>cce', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code' },
-      { '<leader>cct', '<cmd>CopilotChatTests<cr>', desc = 'CopilotChat - Generate tests' },
-      { '<leader>ccr', '<cmd>CopilotChatReview<cr>', desc = 'CopilotChat - Review code' },
-      { '<leader>ccR', '<cmd>CopilotChatRefactor<cr>', desc = 'CopilotChat - Refactor code' },
-      { '<leader>ccf', '<cmd>CopilotChatFixCode<cr>', desc = 'CopilotChat - Fix code' },
-      { '<leader>ccb', '<cmd>CopilotChatBetterNamings<cr>', desc = 'CopilotChat - Better Name' },
-      { '<leader>ccd', '<cmd>CopilotChatDocumentation<cr>', desc = 'CopilotChat - Add documentation for code' },
-      -- Text related commands
-      { '<leader>ccs', '<cmd>CopilotChatSummarize<cr>', desc = 'CopilotChat - Summarize text' },
-      { '<leader>ccS', '<cmd>CopilotChatSpelling<cr>', desc = 'CopilotChat - Correct spelling' },
-      { '<leader>ccw', '<cmd>CopilotChatWording<cr>', desc = 'CopilotChat - Improve wording' },
-      { '<leader>ccc', '<cmd>CopilotChatConcise<cr>', desc = 'CopilotChat - Make text concise' },
-      -- Chat with Copilot in visual mode
-      {
-        '<leader>ccv',
-        ':CopilotChatVisual',
-        mode = 'x',
-        desc = 'CopilotChat - Open in vertical split',
-      },
-      {
-        '<leader>ccx',
-        ':CopilotChatInPlace<cr>',
-        mode = 'x',
-        desc = 'CopilotChat - Run in-place code',
-      },
       -- Custom input for CopilotChat
-      {
-        '<leader>cci',
-        function()
+      { '<leader>aa', function()
           local input = vim.fn.input('Ask Copilot: ')
           if input ~= '' then
             vim.cmd('CopilotChat ' .. input)
           end
         end,
-        desc = 'CopilotChat - Ask input',
+        desc = 'Ask for something else',
       },
+      -- Code related commands
+      { '<leader>ab', '<cmd>CopilotChatBetterNamings<cr>', desc = 'Better Name' },
+      { '<leader>ac', '<cmd>CopilotChatConcise<cr>', desc = 'Make text concise' },
+      { '<leader>ad', '<cmd>CopilotChatDocumentation<cr>', desc = 'Add documentation for code' },
+      { '<leader>aD', '<cmd>CopilotChatDebugInfo<cr>', desc = 'Debug Info' },
+      { '<leader>ae', '<cmd>CopilotChatExplain<cr>', desc = 'Explain code' },
+      { '<leader>af', '<cmd>CopilotChatFixCode<cr>', desc = 'Fix code' },
+      { '<leader>ai', ':CopilotChatInPlace<cr>', mode = 'x', desc = 'Discuss selection in float' },
+      { '<leader>al', '<cmd>CopilotChatSpelling<cr>', desc = 'Correct spelling' },
+      { '<leader>aw', '<cmd>CopilotChatWording<cr>', desc = 'Improve wording' },
+      { '<leader>ar', '<cmd>CopilotChatReview<cr>', desc = 'Review code' },
+      { '<leader>aR', '<cmd>CopilotChatRefactor<cr>', desc = 'Refactor code' },
+      { '<leader>as', ':CopilotChatVisual', mode = 'x', desc = 'Discuss selection in split' },
+      { '<leader>at', '<cmd>CopilotChatTests<cr>', desc = 'Generate tests' },
+      { '<leader>az', '<cmd>CopilotChatSummarize<cr>', desc = 'Summarize text' },
+      
       -- Generate commit message base on the git diff
       {
-        '<leader>ccm',
+        '<leader>am',
         function()
           local diff = get_git_diff()
           if diff ~= '' then
@@ -79,10 +94,10 @@ return {
             vim.cmd('CopilotChat Write commit message for the change with commitizen convention.')
           end
         end,
-        desc = 'CopilotChat - Generate commit message for all changes',
+        desc = 'Generate commit message for all changes',
       },
       {
-        '<leader>ccM',
+        '<leader>aM',
         function()
           local diff = get_git_diff(true)
           if diff ~= '' then
@@ -90,10 +105,8 @@ return {
             vim.cmd('CopilotChat Write commit message for the change with commitizen convention.')
           end
         end,
-        desc = 'CopilotChat - Generate commit message for staged changes',
+        desc = 'Generate commit message for staged changes',
       },
-      -- Debug
-      { '<leader>ccD', '<cmd>CopilotChatDebugInfo<cr>', desc = 'CopilotChat - Debug Info' },
     },
   },
 }
