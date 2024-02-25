d3f997917a3f73a36b3ecee097da9f3f840d351f
Michael Uloth <hello@michaeluloth.com>
2024-02-25 09:58:16 -0500

    wip(neovim): improve TODO highlighting style

--- config/nvim-lazyvim/lua/plugins/searching/todo-comments.lua
+++ config/nvim-lazyvim/lua/plugins/searching/todo-comments.lua
@@ -6,6 +6,14 @@ return {
         { '<leader>st', '<cmd>TodoTelescope<cr>', desc = 'Todo' },
       }
     end,
+    opts = {
+      -- FIXME: doesn't do anything yet
+      -- see: https://github.com/folke/todo-comments.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+      gui_style = {
+        fg = 'BOLD', -- The gui style to use for the fg highlight group.
+        bg = 'NONE', -- The gui style to use for the bg highlight group.
+      },
+    },
     -- FIXME: doesn't solve the problem of TODO/FIXME/NOTE not tinting
     -- init = function()
     --   require('tint').refresh()
