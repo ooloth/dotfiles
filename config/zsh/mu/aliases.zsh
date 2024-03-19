756f59012a1cd43b0edaf1a5c56de6f9d713d599
Michael Uloth <hello@michaeluloth.com>
2024-03-19 10:57:09 -0400

    fix: dc should accept multiple args

--- config/zsh/mu/aliases.zsh
+++ config/zsh/mu/aliases.zsh
@@ -16,10 +16,12 @@ alias da='docker container ls --all --format "table {{.ID}}\t{{.Names}}\t{{.Stat
 alias dash='cd $HOME/Repos/ooloth/dashboard'
 de() { docker container exec -it $1 sh; }
 
-alias dc='docker compose'
+# TODO: improve defaults
+dc() { docker compose "$@"; }
 alias dd='dc down --remove-orphans'                # stop and remove one or more containers, networks, images, and volumes (or all if no args provided)
 alias dl='dc logs --follow --tail=100'             # see last 100 log lines of one or more services (or all services if no args provided)
 alias du='dc up --build --detach --remove-orphans' # recreate and start one or more services (or all services if no args provided)
+alias dud='dc up --detach'                         # start one or more services (or all services if no args provided)
 
 alias env='env | sort'
 alias f='vifm'
