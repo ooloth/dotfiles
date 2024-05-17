490d4bd70897c4d0df01542eaf0690431fc09801
Michael Uloth <hello@michaeluloth.com>
2024-05-17 11:34:42 -0400

    symlinks: start a script to run to refresh all links

---
+++ bin/create-symlinks.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+local dotfiles=$HOME/Repos/ooloth/dotfiles
+
+# -s: symbolic - create a symbolic link (not a hard link)
+# -f: force - if the target file already exists, then unlink it so that the link may occur
+# -v: verbose - print file names as they are processed
+sl() { ln -sfv $1 $2; } # easier symlinking
+
+#########
+# KITTY #
+#########
+
+# see: https://github.com/knubie/vim-kitty-navigator?tab=readme-ov-file#kitty
+sl $dotfiles/config/kitty/vim-kitty-navigator/get_layout.py $HOME/.config/kitty
+sl $dotfiles/config/kitty/vim-kitty-navigator/pass_keys.py $HOME/.config/kitty
