2dac47bb4940ce16bf04a6b24c60b36939176099
Michael Uloth <hello@michaeluloth.com>
2024-05-17 15:39:09 -0400

    aliases: refactor banner, info, warn and error for readability

--- config/zsh/mu/aliases.zsh
+++ config/zsh/mu/aliases.zsh
@@ -4,15 +4,13 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
 function banner() {
-  # Args:
-  # $1 message: The message to display
-  # $2 color: The color of the border and text
+  # Capture the text and color arguments
+  local text="$1"
+  local color="$2"
 
-  local message="$1"
-  local border_color="${TEXT_NORMAL}$2"
-  local text_color="${TEXT_BRIGHT}$2"
-
-  # Calculate the width of the text, adding one extra column per emoji (since they generally occupy two columns onscreen)
+  # Define the text and border styles
+  local text_color="${TEXT_BRIGHT}$color"
+  local border_color="${TEXT_NORMAL}$color"
   local border_char="="
   local border_char_top_left_corner="╔"
   local border_char_top_right_corner="╗"
@@ -20,38 +18,40 @@ function banner() {
   local border_char_bottom_left_corner="╚"
   local border_char_horizontal="═"
   local border_char_vertical="║"
-  local char_count=${#message}
-  local emoji_count=$(echo -n "$message" | python3 -c "import sys, unicodedata; print(sum((unicodedata.category(ch) == 'So') for ch in sys.stdin.read()))")
+
+  # Calculate the width of the text, adding one extra column per emoji (since they generally occupy two columns onscreen)
+  local char_count=${#text}
+  local emoji_count=$(echo -n "$text" | python3 -c "import sys, unicodedata; print(sum((unicodedata.category(ch) == 'So') for ch in sys.stdin.read()))")
   local padding_left=1
   local padding_right=1
   local text_cols=$((padding_left + char_count + emoji_count + padding_right))
 
-  # Build the banner
+  # Build the banner components
   local border_top="$border_color$border_char_top_left_corner$(repeat $text_cols; printf $border_char_horizontal)$border_char_top_right_corner"
   local border_vertical="$border_color$border_char_vertical"
   local border_bottom="$border_color$border_char_bottom_left_corner$(repeat $text_cols; printf $border_char_horizontal)$border_char_bottom_right_corner"
-  local text="$text_color $message "
+  local text="$text_color $text "
 
-  # Output the banner
+  # Output the assembled banner
   printf "\n$border_top\n$border_vertical$text$border_vertical\n$border_bottom\n\n"
 }
 
 function info() {
-  local message="$1"
+  local text="$1"
   local color="${TEXT_WHITE}"
-  banner "$message" "$color"
+  banner $text $color
 }
 
 function warn() {
-  local message="$1"
+  local text="$1"
   local color="${TEXT_YELLOW}"
-  banner "$message" "$color"
+  banner $text $color
 }
 
 function error() {
-  local message="$1"
+  local text="$1"
   local color="${TEXT_RED}"
-  banner "$message" "$color"
+  banner $text $color
 }
 
 alias c='clear'
