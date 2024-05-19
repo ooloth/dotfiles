3c5dd035f6e10d6775345da91fc83adcfd6257e1
Michael Uloth <hello@michaeluloth.com>
2024-05-18 20:28:53 -0400

    zsh: remove reference to starship

--- config/zsh/mu/plugins.zsh
+++ config/zsh/mu/plugins.zsh
@@ -4,11 +4,13 @@ eval "$(fnm env --use-on-cd --log-level=error)"
 # fzf
 eval "$(fzf --zsh)"
 
-# prompt
+# github-copilot-cli
+eval "$(github-copilot-cli alias -- "$0")"
+
+# powerlevel10k
 # see: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#homebrew
 source $(brew --prefix)/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme
 source $HOME/.config/zsh/mu/.p10k.zsh # to customize, run `p10k configure` or edit config/zsh/mu/.p10k.zsh
-# eval "$(starship init zsh)"
 
 # zsh-autosuggestions
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
@@ -16,8 +18,6 @@ source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 # zsh-syntax-highlighting
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-# github-copilot-cli
-eval "$(github-copilot-cli alias -- "$0")"
 if $IS_WORK_LAPTOP; then
   # Gcloud (command completion)
   if [ -f '/Users/michael.uloth/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/michael.uloth/google-cloud-sdk/completion.zsh.inc'; fi
