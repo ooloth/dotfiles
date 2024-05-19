86bc6b75fe9383630157a42d3480eae69730d967
Michael Uloth <hello@michaeluloth.com>
2024-05-18 20:23:13 -0400

    symlinks: add kitty.environment.plist

--- bin/create-symlinks.zsh
+++ bin/create-symlinks.zsh
@@ -13,3 +13,9 @@ sl() { ln -sfv $1 $2; } # easier symlinking
 # see: https://github.com/knubie/vim-kitty-navigator?tab=readme-ov-file#kitty
 sl $HOME/Repos/knubie/vim-kitty-navigator/get_layout.py $HOME/.config/kitty
 sl $HOME/Repos/knubie/vim-kitty-navigator/pass_keys.py $HOME/.config/kitty
+
+# set HOSTNAME for kitty startup config swapping
+# see: https://github.com/kovidgoyal/kitty/issues/811#issuecomment-414186903
+# see: https://github.com/kovidgoyal/kitty/issues/811#issuecomment-434876639
+# see: https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.startup_session
+sl $DOTFILES/macos/kitty.environment.plist $HOME/Library/LaunchAgents
