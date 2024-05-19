8b35d544e09ccebba9bf2838d0ea74777fa5771e
Michael Uloth <hello@michaeluloth.com>
2024-05-19 15:29:12 -0400

    symlinks: organize by ~, ~/config and Library targets

--- bin/create-symlinks.zsh
+++ bin/create-symlinks.zsh
@@ -7,27 +7,18 @@ local HOMECONFIG=$HOME/.config
 
 local sl() { ln -sfv "$1" "$2"; }
 
-sl $DOTFILES/.hushlogin $HOME
 #####
 # ~ #
 #####
 
 
-# see: https://github.com/knubie/vim-kitty-navigator?tab=readme-ov-file#kitty
-sl $HOME/Repos/knubie/vim-kitty-navigator/get_layout.py $HOMECONFIG/kitty
-sl $HOME/Repos/knubie/vim-kitty-navigator/pass_keys.py $HOMECONFIG/kitty
-# set HOSTNAME for kitty startup config swapping
-# see: https://github.com/kovidgoyal/kitty/issues/811#issuecomment-414186903
-# see: https://github.com/kovidgoyal/kitty/issues/811#issuecomment-434876639
-# see: https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.startup_session
-sl $DOTFILES/macos/kitty.environment.plist $HOME/Library/LaunchAgents
-
+sl $DOTFILES/.hushlogin $HOME
+sl $DOTFILES/.zshenv $HOME
 
 #############
 # ~/.config #
 #############
 
-sl $DOTFILES/.npmrc $HOME
 
 # Find absolute paths to all files at any level under $DOTCONFIG (see: https://github.com/sharkdp/fd)
 fd --type file . $DOTCONFIG | while read file; do
@@ -44,13 +35,27 @@ fd --type file . $DOTCONFIG | while read file; do
   sl "$file" "$HOMECONFIG/$dirpath"
 done
 
+# TODO: clone first if missing?
+# see: https://github.com/knubie/vim-kitty-navigator?tab=readme-ov-file#kitty
+sl $HOME/Repos/knubie/vim-kitty-navigator/get_layout.py $HOMECONFIG/kitty
+sl $HOME/Repos/knubie/vim-kitty-navigator/pass_keys.py $HOMECONFIG/kitty
 
-sl $DOTFILES/vscode/settings.json "$HOME/Library/Application Support/Code/User"
-sl $DOTFILES/vscode/keybindings.json "$HOME/Library/Application Support/Code/User"
-sl $DOTFILES/vscode/snippets "$HOME/Library/Application Support/Code/User"
 # TODO: clone yazi flavors repo elsewhere and symlink to ~ from there?
 # sl $DOTCONFIG/yazi/flavors $HOMECONFIG/yazi
 
+#############
+# ~/Library #
+#############
 
+local LAUNCHAGENTS="$HOME/Library/LaunchAgents"
+local VSCODEUSER="$HOME/Library/Application Support/Code/User"
 
+# set HOSTNAME for kitty startup config swapping
+# see: https://github.com/kovidgoyal/kitty/issues/811#issuecomment-414186903
+# see: https://github.com/kovidgoyal/kitty/issues/811#issuecomment-434876639
+# see: https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.startup_session
+sl $DOTFILES/macos/kitty.environment.plist $LAUNCHAGENTS
 
+sl $DOTFILES/vscode/settings.json $VSCODEUSER
+sl $DOTFILES/vscode/keybindings.json $VSCODEUSER
+sl $DOTFILES/vscode/snippets $VSCODEUSER
