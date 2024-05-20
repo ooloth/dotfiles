91a434a608be45912e5682219c9731eac8c323ad
Michael Uloth <hello@michaeluloth.com>
2024-05-20 15:27:14 -0400

    zsh("u"): clean up brew files not listed in brewfile when updating

--- config/zsh/update.zsh
+++ config/zsh/update.zsh
@@ -1,6 +1,6 @@
 u() {
   info "🔗 Updating symlinks"
-  $DOTFILES/bin/create-symlinks.zsh
+  source "$DOTFILES/bin/create-symlinks.zsh"
 
   info "✨ Updating rust dependencies"
   rustup update
@@ -12,7 +12,7 @@ u() {
   ~/.config/tmux/plugins/tpm/bin/update_plugins all
 
   info "✨ Updating neovim dependencies"
-  git -C $HOME/Repos/knubie/vim-kitty-navigator pull;
+  git -C "$HOME/Repos/knubie/vim-kitty-navigator" pull;
 
   # TODO: update lazy.nvim plugins here as well? in all nvim instances? pin dependencies to avoid unwanted updates?
 
@@ -29,11 +29,7 @@ u() {
 
   info "✨ Updating brew packages"
   # Install missing packages, upgrade outdated packages, and remove old versions
-  # TODO: maintain separate Brewfiles for work laptop, personal laptop and Mini?
-  # Ok to vary the file name?
-  # If not, can I symlink the Brewfile to the appropriate one based on the $HOSTNAME?
-  # Reference the appropriate one by interpolating the $HOSTNAME?
-  brew bundle --file=$DOTFILES/macos/Brewfile
+  brew bundle --file="$DOTFILES/macos/Brewfile" --cleanup
 	brew update && brew upgrade && brew autoremove && brew cleanup && brew doctor
 
   # Avoid potential issues on work laptop caused by updating macOS too early
