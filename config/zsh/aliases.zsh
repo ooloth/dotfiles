eff73d69e8c5b8e02a1be00bbc4a2495b37d1fe7
Michael Uloth <hello@michaeluloth.com>
2024-05-20 21:53:48 -0400

    zsh(u): only install missing or outdated npm dependencies

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -66,13 +66,57 @@ nd() { n && npm run dev; }
 nfc() { npm run format:check; }
 nff() { npm run format:fix; }
 ng() {
-  # prefer "-g" over "--location=global" to support older versions of npm
+  packages=(
+    aocrunner
+    bash-language-server # for neovim
+    cssmodules-language-server # for neovim
+    dockerfile-language-server-nodejs # for neovim
+    emmet-ls # for neovim
+    neovim # for neovim
+    npm
     npm-check
+    pug-lint # for neovim
+    svelte-language-server # for neovim
+    tree-sitter-cli # for neovim
+    typescript # for neovim
+    vscode-langservers-extracted # for neovim
+  )
 
-  # neovim dependencies
-  npm install -g \
-    typescript \
-    vscode-langservers-extracted
+  installed_packages=$(npm list -g --depth=0)
+  outdated_packages=$(npm outdated -g)
+
+  is_package_installed() {
+    local package="$1"
+    echo "$installed_packages" | grep -q " ${package}@"
+  }
+
+  is_package_outdated() {
+    local package="$1"
+    echo "$outdated_packages" | grep -q "^${package}"
+  }
+
+  packages_to_add=()
+  packages_to_update=()
+
+  for package in "${packages[@]}"; do
+      if ! is_package_installed "$package"; then
+        packages_to_add+=("$package")
+      elif is_package_outdated "$package"; then
+        packages_to_update+=("$package")
+      fi
+  done
+
+  echo
+  for package in "${packages_to_add[@]}"; do
+    printf "📦 Installing %s\n" "$package"
+  done
+
+  for package in "${packages_to_update[@]}"; do
+    printf "🚀 Updating %s\n" "$package"
+  done
+
+  # prefer "-g" over "--location=global" to support older versions of npm
+  npm install -g --loglevel=error "${packages_to_add[@]}" "${packages_to_update[@]}"
 }
 nk() { npm run types:check; }
 nl() { npm run lint; }
