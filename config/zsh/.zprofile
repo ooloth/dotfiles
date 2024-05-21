5a3f53c7e3170874f2fc400b3a94cada050aaf2b
Michael Uloth <hello@michaeluloth.com>
2024-05-20 20:12:05 -0400

    zsh: try moving PATH from zprofile to variables

--- config/zsh/.zprofile
+++ config/zsh/.zprofile
@@ -10,27 +10,3 @@ if $IS_WORK_LAPTOP; then
   # see: https://github.com/recursionpharma/data-science-onboarding#setting-up-griphook
   source $HOME/.griphook/env
 fi
-
-########
-# PATH #
-########
-
-if $IS_WORK_LAPTOP; then
-  # Gcloud (update PATH for the Google Cloud SDK)
-  # see: https://cloud.google.com/sdk/docs/downloads-interactive
-  if [ -f '/Users/michael.uloth/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/michael.uloth/google-cloud-sdk/path.zsh.inc'; fi
-fi
-
-# Pyenv
-# NOTE: do NOT use eval "$(pyenv init -)" or eval "$(pyenv virtualenv-init -)" (they slow the shell down a lot)
-# see: https://github.com/pyenv/pyenv?tab=readme-ov-file#set-up-your-shell-environment-for-pyenv
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-
-# openSSL
-export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
-
-# Rust
-export PATH="$HOME/.config/cargo/bin:$PATH"
-
-# Homebrew (keep last so will be at front of PATH)
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:$PATH" # Add Homebrew's executable directory to front of PATH
--- config/zsh/variables.zsh
+++ config/zsh/variables.zsh
@@ -81,7 +81,34 @@ export XDG_CONFIG_HOME=$HOME/.config
 # Zsh
 export ZDOTDIR=$HOME/.config/zsh
 
-# Work
+########
+# PATH #
+########
+
+if $IS_WORK_LAPTOP; then
+  # Gcloud (update PATH for the Google Cloud SDK)
+  # see: https://cloud.google.com/sdk/docs/downloads-interactive
+  if [ -f '/Users/michael.uloth/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/michael.uloth/google-cloud-sdk/path.zsh.inc'; fi
+fi
+
+# Pyenv
+# NOTE: do NOT use eval "$(pyenv init -)" or eval "$(pyenv virtualenv-init -)" (they slow the shell down a lot)
+# see: https://github.com/pyenv/pyenv?tab=readme-ov-file#set-up-your-shell-environment-for-pyenv
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+
+# openSSL
+export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
+
+# Rust
+export PATH="$HOME/.config/cargo/bin:$PATH"
+
+# Homebrew (keep last so will be at front of PATH)
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:$PATH" # Add Homebrew's executable directory to front of PATH
+
+########
+# WORK #
+########
+
 if $IS_WORK_LAPTOP; then
   # Configome
   # see: https://app.swimm.io/workspaces/ctl2gE0Uy2okwcN3LfOg/repos/Z2l0aHViJTNBJTNBaXBnLW9yY2hlc3RyYXRvciUzQSUzQXJlY3Vyc2lvbnBoYXJtYQ==/branch/trunk/docs/v0jze
