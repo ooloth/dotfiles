f231c3d96a803f3fad27b2a2fa27df31234a1a98
Michael Uloth <hello@michaeluloth.com>
2024-05-22 21:37:43 -0400

    update paths

--- config/yazi/flavors/README.md
+++ config/yazi/flavors/README.md
@@ -1,3 +1,3 @@
-Yazi theme files will be symlinked to `$HOME/.config/yazi/flavors` from `$HOME/Repos/yazi-rs/flavors`. See `bin/create-symlinks.sh` to automatic symlink more flavors so they'll be available.
+Yazi theme files will be symlinked to `$HOME/.config/yazi/flavors` from `$HOME/Repos/yazi-rs/flavors`. See `bin/update/symlinks.sh` to automatic symlink more flavors so they'll be available.
 
 This empty folder is here to ensure `$HOME/.config/yazi/flavors` will be automatically created.
--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -7,14 +7,14 @@ eval "$(fzf --zsh)"
 
 # powerlevel10k
 # see: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#homebrew
-source "$(brew --prefix)/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme"
+source "/opt/homebrew/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme"
 source "$HOME/.config/zsh/p10k.zsh" # to customize, run `p10k configure` or edit config/zsh/p10k.zsh
 
 # zsh-autosuggestions
-source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 # zsh-syntax-highlighting
-source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 if $IS_WORK; then
   # Gcloud (command completion)
