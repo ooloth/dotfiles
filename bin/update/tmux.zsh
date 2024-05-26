8f0446c5a3f0644a8f905a227b9277d4afca2a1a
Michael Uloth <hello@michaeluloth.com>
2024-05-26 19:43:25 -0400

    update(tmux): capitalize static vars

--- bin/update/tmux.zsh
+++ bin/update/tmux.zsh
@@ -1,21 +1,21 @@
 #!/usr/bin/env zsh
 
-tpm_path="$HOME/.config/tmux/plugins/tpm"
-dotfiles="$HOME/Repos/ooloth/dotfiles"
+TPM="$HOME/.config/tmux/plugins/tpm"
+DOTFILES="$HOME/Repos/ooloth/dotfiles"
 
 # Install if missing
-if [ ! -d "$tpm_path" ]; then
-  source "$dotfiles/bin/install/tmux.zsh"
+if [ ! -d "$TPM" ]; then
+  source "$DOTFILES/bin/install/tmux.zsh"
 fi
 
 # Then, update
-source "$dotfiles/config/zsh/banners.zsh"
+source "$DOTFILES/config/zsh/banners.zsh"
 info "✨ Updating tmux dependencies"
 
 # see: https://github.com/tmux-plugins/tpm/blob/master/docs/managing_plugins_via_cmd_line.md
-"$tpm_path/bin/clean_plugins"
-"$tpm_path/bin/install_plugins"
-"$tpm_path/bin/update_plugins" all
+"$TPM/bin/clean_plugins"
+"$TPM/bin/install_plugins"
+"$TPM/bin/update_plugins" all
 
 # Reload tmux config automatically if it's running
 if pgrep -q tmux; then
