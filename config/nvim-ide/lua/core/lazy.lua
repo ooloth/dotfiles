d744e02617b743874c114b846de16a2183f5fb72
Michael Uloth <hello@michaeluloth.com>
2024-06-15 22:25:08 -0400

    nvim-ide: add lazy

---
+++ config/nvim-ide/lua/core/lazy.lua
@@ -0,0 +1,23 @@
+-- see: https://github.com/folke/lazy.nvim?tab=readme-ov-file#-installation
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- see: https://github.com/folke/lazy.nvim?tab=readme-ov-file#-structuring-your-plugins
+require('lazy').setup({
+  spec = {
+    { import = 'lang' },
+    { import = 'plugins' },
+  },
+})
