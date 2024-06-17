abc5877cfbcefc6b5e4879e29b7a9fb57d82a645
Michael Uloth <hello@michaeluloth.com>
2024-06-17 19:30:31 -0400

    nvim-ide(rust): add formatting

---
+++ config/nvim-ide/lua/lang/rust.lua
@@ -0,0 +1,54 @@
+--  TODO: lsp
+--  TODO: linting
+--  TODO: dap
+
+return {
+  --  {
+  --   'williamboman/mason.nvim',
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed or {}, {})
+  --   end,
+  -- },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed or {}, { 'rust' })
+    end,
+  },
+
+  -- formatting (see: https://github.com/stevearc/conform.nvim#setup)
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        rust = { 'rustfmt' },
+      },
+    },
+  },
+
+  {
+    -- see: https://github.com/mrcjkb/rustaceanvim?tab=readme-ov-file#inbox_tray-installation
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {},
+        -- LSP configuration
+        server = {
+          on_attach = function(client, bufnr)
+            -- you can also put keymaps in here
+          end,
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {},
+          },
+        },
+        -- DAP configuration
+        dap = {},
+      }
+    end,
+  },
+}
