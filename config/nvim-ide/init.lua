52ed0847b2b542d06d2fcd0133f7228db1de8b76
Michael Uloth <hello@michaeluloth.com>
2024-06-16 20:12:34 -0400

    nvim-ide: add autocmds

--- config/nvim-ide/init.lua
+++ config/nvim-ide/init.lua
@@ -1,3 +1,4 @@
 require('core.keymaps')
 require('core.options')
 require('core.lazy')
+require('core.autocmds')
---
+++ config/nvim-ide/lua/core/autocmds.lua
@@ -0,0 +1,30 @@
+-- see: https://neovim.io/doc/user/autocmd.html
+
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   callback = function()
+--     require("persistence").load()
+--   end,
+-- })
+
+vim.cmd([[
+  autocmd InsertEnter * set nocursorline
+  autocmd InsertLeave * set cursorline
+]])
+
+-- override nvim runtime filetype "ro" settings that add a comment leader to newlines below a comment:
+-- https://neovim.discourse.group/t/options-formatoptions-not-working-when-put-in-init-lua/3746/5
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    vim.opt.formatoptions:remove('r')
+    vim.opt.formatoptions:remove('o')
+  end,
+})
+
+-- TODO: wait for lsp server to attach (move to lsp.lua?)
+-- show line diagnostics in floating window while cursor is on line
+-- see: https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
+vim.api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false })
+  end,
+})
