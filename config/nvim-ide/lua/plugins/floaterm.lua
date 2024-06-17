9d8cdcb5a605ff097e9bdd9b6291794646b74cdd
Michael Uloth <hello@michaeluloth.com>
2024-06-17 16:47:52 -0400

    nvim-ide(floaterm): no borders

--- config/nvim-ide/lua/plugins/floaterm.lua
+++ config/nvim-ide/lua/plugins/floaterm.lua
@@ -1,3 +1,7 @@
+-- TODO: toggle running lazygit instance instead of quitting and restarting lazygit each time?
+-- TODO: command! Yazi FloatermNew yazi
+
+vim.g.floaterm_borderchars = ''
 vim.g.floaterm_height = 0.9999999999999999
 vim.g.floaterm_title = ''
 vim.g.floaterm_width = 0.9999999999999999
@@ -9,7 +13,6 @@ return {
     cmd = 'FloatermNew',
     keys = {
       { '<leader>gg', '<cmd>FloatermNew lazygit<cr>', desc = 'Lazygit' },
-      -- command! Yazi FloatermNew yazi
     },
   },
 }
