d3833f54e8bf0a5d7853ea51deab5de2dae6edaa
Michael Uloth <hello@michaeluloth.com>
2024-06-16 20:14:56 -0400

    nvim-ide: add lua support

---
+++ config/nvim-ide/lua/lang/lua.lua
@@ -0,0 +1,42 @@
+-- TODO: set up the `lua_ls` language server:
+-- https://www.lazyvim.org/plugins/lsp#nvim-lspconfig
+
+return {
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed or {}, { 'stylua' })
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed or {}, { 'lua', 'luadoc', 'luap' })
+    end,
+  },
+
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+      },
+    },
+  },
+
+  -- {
+  --   -- see: https://github.com/mfussenegger/nvim-lint
+  --   'mfussenegger/nvim-lint',
+  --   opts = {
+  --     -- see: https://www.lazyvim.org/plugins/linting#nvim-lint
+  --     -- see: https://github.com/mfussenegger/nvim-lint#available-linters
+  --     linters_by_ft = {
+  --       lua = { 'luacheck' },
+  --     },
+  --   },
+  -- },
+}
+
+--  TODO: linting
+--  TODO: dap?
