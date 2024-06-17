c4872ad0961b4d1491afb7f486f0d4c846e49e73
Michael Uloth <hello@michaeluloth.com>
2024-06-16 20:16:03 -0400

    nvim-ide: add filetype associations

--- config/nvim-ide/lua/core/options.lua
+++ config/nvim-ide/lua/core/options.lua
@@ -33,3 +33,25 @@ vim.cmd([[
   let g:loaded_perl_provider = 0
   let g:loaded_ruby_provider = 0
 ]])
+
+-- custom filename -> filetype associations
+vim.filetype.add({
+  extension = {
+    -- see: https://sbulav.github.io/vim/neovim-improving-work-with-terraform/#correctly-detecting-tf-filetype
+    tf = 'terraform',
+    tfvars = 'terraform',
+    tfstate = 'json',
+  },
+  filename = {
+    ['tsconfig.json'] = 'jsonc',
+    -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#docker_compose_language_service
+    ['docker-compose.yaml'] = 'yaml.docker-compose',
+  },
+  pattern = {
+    ['docker-compose.*%.yaml'] = 'yaml.docker-compose',
+    ['.*/kitty/.*%.conf'] = 'bash',
+    ['.*/kitty/.*/.*%.conf'] = 'bash',
+    ['.*/.vscode/.*%.json'] = 'jsonc',
+    ['.*/vscode/.*%.json'] = 'jsonc',
+  },
+})
