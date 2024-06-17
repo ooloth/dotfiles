ba8b9670db04ac639fc0f8b820cdfc287e180097
Michael Uloth <hello@michaeluloth.com>
2024-06-16 20:14:12 -0400

    nvim-ide: add vim-tmux-navigator

---
+++ config/nvim-ide/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,20 @@
+-- navigate vim splits (and tmux panes) with <C-hjkl>
+
+return {
+  'christoomey/vim-tmux-navigator',
+  event = 'VeryLazy',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    -- 'TmuxNavigatePrevious',
+  },
+  keys = {
+    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    -- { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+  },
+}
