2d95533d1b8b8fdd2d8ecc192f604c4135877fe4
Michael Uloth <hello@michaeluloth.com>
2024-06-18 17:17:05 -0400

    nvim(floaterm): c-t to open a scratch terminal in a float

--- config/nvim-ide/lua/plugins/floaterm.lua
+++ config/nvim-ide/lua/plugins/floaterm.lua
@@ -5,9 +5,9 @@
 -- TODO: command! Yazi FloatermNew yazi
 
 vim.g.floaterm_borderchars = ''
-vim.g.floaterm_height = 0.9999999999999999
+vim.g.floaterm_height = 0.75
 vim.g.floaterm_title = ''
-vim.g.floaterm_width = 0.9999999999999999
+vim.g.floaterm_width = 0.75
 
 return {
   {
@@ -20,11 +20,14 @@ return {
     init = function()
       -- On startup, start lazygit in a hiddle terminal so it's ready to appear instantly
       -- https://github.com/voldikss/vim-floaterm?tab=readme-ov-file#commands
-      vim.cmd([[ FloatermNew --silent --name=lazygit lazygit ]])
+      vim.cmd([[ FloatermNew --silent --name=lazygit --height=0.9999999999999999 --width=0.9999999999999999 lazygit ]])
 
       -- Use 'esc' or 'q' to hide floaterm without stopping the process running in it
       vim.keymap.set('t', '<esc>', '<cmd>FloatermHide<cr>', { desc = 'Hide terminal' })
       vim.keymap.set('t', 'q', '<cmd>FloatermHide<cr>', { desc = 'Hide terminal' })
+
+      -- stylua: ignore
+      vim.keymap.set('n', '<c-t>', '<cmd>FloatermNew --disposable --borderchars=─│─│┌┐┘└<cr>', { desc = 'New terminal' })
     end,
   },
 }
