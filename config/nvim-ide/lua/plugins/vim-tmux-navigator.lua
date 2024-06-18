f449e53b4acfcde09b068ea4f782de2c3460e7ca
Michael Uloth <hello@michaeluloth.com>
2024-06-18 16:25:17 -0400

    nvim(kitty): disable vim-kitty-navigator in favor of vim-tmux-navigator

--- config/nvim-ide/lua/plugins/vim-tmux-navigator.lua
+++ config/nvim-ide/lua/plugins/vim-tmux-navigator.lua
@@ -8,13 +8,13 @@ return {
     'TmuxNavigateDown',
     'TmuxNavigateUp',
     'TmuxNavigateRight',
-    -- 'TmuxNavigatePrevious',
+    'TmuxNavigatePrevious',
   },
   keys = {
     { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
     { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
     { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
     { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-    -- { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
   },
 }
