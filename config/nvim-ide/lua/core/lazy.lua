5e3d184498aa8a41fbed44467dc25d2b7751eb55
Michael Uloth <hello@michaeluloth.com>
2024-06-19 07:56:08 -0400

    nvim(lazy): install stable versions and don't notify when changes detected

--- config/nvim-ide/lua/core/lazy.lua
+++ config/nvim-ide/lua/core/lazy.lua
@@ -14,10 +14,64 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
--- see: https://github.com/folke/lazy.nvim?tab=readme-ov-file#-structuring-your-plugins
 require('lazy').setup({
+  -- see: https://github.com/folke/lazy.nvim?tab=readme-ov-file#-structuring-your-plugins
   spec = {
     { import = 'lang' }, -- all files in lua/lang
     { import = 'plugins' }, -- all files in lua/plugins
+  },
+
+  change_detection = {
+    notify = false,
+  },
+
+  checker = {
+    notify = false,
+  },
+
+  -- see: https://github.com/folke/lazy.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+  defaults = {
+    version = '*', -- enable this to try installing the latest stable versions of plugins
+  },
+
+  diff = {
+    -- diff command <d> can be one of:
+    -- * browser: opens the github compare view. Note that this is always mapped to <K> as well,
+    --   so you can have a different command for diff <d>
+    -- * git: will run git diff and open a buffer with filetype git
+    -- * terminal_git: will open a pseudo terminal with git diff
+    -- * diffview.nvim: will open Diffview to show the diff
+    cmd = 'git',
+  },
+
+  install = {
+    colorscheme = {},
+  },
+
+  performance = {
+    rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      ---@type string[] list any plugins you want to disable here
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
+      },
+    },
+  },
+
+  -- Enable profiling of lazy.nvim. This will add some overhead,
+  -- so only enable this when you are debugging lazy.nvim
+  profiling = {
+    -- Enables extra stats on the debug tab related to the loader cache.
+    -- Additionally gathers stats about all package.loaders
+    loader = false,
+    -- Track each new require in the Lazy profiling tab
+    require = false,
   },
 })
