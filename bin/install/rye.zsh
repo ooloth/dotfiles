1c571b72b67a8ed4353781b8015209b53ad3a8f0
Michael Uloth <hello@michaeluloth.com>
2024-07-29 09:51:14 -0400

    update/setup: install rye if missing

---
+++ bin/install/rye.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+# Return if installed
+if command -v rye &> /dev/null; then
+  printf "\n🌾 Rye is already installed\n"
+  return
+fi
+
+# Otherwise, install
+source "$HOME/Repos/ooloth/dotfiles/config/zsh/banners.zsh"
+info "🌾 Installing rye"
+
+# Use custom paths
+export RYE_HOME="$HOME/.config/rye"
+
+# See: https://rye.astral.sh/guide/installation/
+curl -sSf https://rye.astral.sh/get | bash
+
+printf "\n🚀 Finished installing rye.\n"
