8c86385c6e771e896aa291456994bd9e47a99f2a
Michael Uloth <hello@michaeluloth.com>
2024-10-14 21:58:04 -0400

    nvim: autoformatting + enable nvim/tmux window movements

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,46 +1,46 @@
 local opt = vim.opt
 
-opt.autowrite = true          -- Enable auto write
+opt.autowrite = true -- Enable auto write
 opt.clipboard = 'unnamedplus' -- Sync with system clipboard
 -- opt.cmdheight = 0
 opt.completeopt = 'menu,menuone,noselect'
-opt.conceallevel = 3           -- Hide * markup for bold and italic
-opt.confirm = true             -- Confirm to save changes before exiting modified buffer
-opt.cursorline = true          -- Enable highlighting of the current line
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true -- Enable highlighting of the current line
 opt.formatoptions = 'jcroqlnt' -- tcqj
 opt.grepformat = '%f:%l:%c:%m'
 opt.grepprg = 'rg --vimgrep'
-opt.ignorecase = true      -- Ignore case
+opt.ignorecase = true -- Ignore case
 opt.inccommand = 'nosplit' -- preview incremental substitute
 opt.laststatus = 0
-opt.list = true            -- Show some invisible characters (tabs...
-opt.mouse = 'a'            -- Enable mouse mode
-opt.number = false         -- Print line number
-opt.pumblend = 10          -- Popup blend
-opt.pumheight = 10         -- Maximum number of entries in a popup
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = 'a' -- Enable mouse mode
+opt.number = false -- Print line number
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = false -- Relative line numbers
-opt.scrolloff = 10         -- Lines of context
+opt.scrolloff = 10 -- Lines of context
 opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize' }
-opt.shiftround = true      -- Round indent
-opt.shiftwidth = 2         -- Size of an indent
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true })
 -- opt.showmode = false       -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8     -- Columns of context
+opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = 'no'
-opt.smartcase = true      -- Don't ignore case with capitals
-opt.smartindent = true    -- Insert indents automatically
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
 opt.spelllang = { 'en' }
-opt.splitbelow = true     -- Put new windows below current
-opt.splitright = true     -- Put new windows right of current
-opt.tabstop = 2           -- Number of spaces tabs count for
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+opt.tabstop = 2 -- Number of spaces tabs count for
 opt.termguicolors = false -- True color support
 opt.timeoutlen = 300
 opt.undofile = true
 opt.undolevels = 10000
-opt.updatetime = 200               -- Save swap file and trigger CursorHold
+opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = 'longest:full,full' -- Command-line completion mode
-opt.winminwidth = 5                -- Minimum window width
-opt.wrap = false                   -- Disable line wrap
+opt.winminwidth = 5 -- Minimum window width
+opt.wrap = false -- Disable line wrap
 
 -- Nicer line highlighting while using Material Ocean theme in terminal
 vim.api.nvim_create_autocmd('ColorScheme', {
@@ -68,9 +68,9 @@ set({ 'n', 'v' }, '<S-l>', '$', { desc = 'Go to end of line', silent = true })
 set({ 'n', 'v' }, '$', '<noop>', { silent = true }) -- practice using L instead
 
 -- "changes"
-set('n', ']c', 'g,', { desc = 'Next change' })     -- go to next change with g;
+set('n', ']c', 'g,', { desc = 'Next change' }) -- go to next change with g;
 set('n', '[c', 'g;', { desc = 'Previous change' }) -- go to next change with g;
-set('n', 'g;', 'g,', { desc = 'Next change' })     -- go to next change with g;
+set('n', 'g;', 'g,', { desc = 'Next change' }) -- go to next change with g;
 set('n', 'g:', 'g;', { desc = 'Previous change' }) -- go to previous change with g;
 
 set('n', '<leader><space>', '<cmd>w<cr>', { desc = 'Save' })
@@ -95,21 +95,21 @@ set('n', '<leader>ww', '<cmd>w<cr>', { desc = 'Write' })
 -- clear search highlights
 set('n', '<esc>', '<cmd>nohlsearch<CR><Esc>', { silent = true })
 
--- TODO: I'm guessing I'd need to actually install vim-kitty-navigator for this to work...?
--- see: https://github.com/christoomey/vim-tmux-navigator/blob/master/plugin/tmux_navigator.vim
-vim.cmd([[
-  noremap <silent> <c-h> :<C-U>KittyNavigateLeft<cr>
-  noremap <silent> <c-j> :<C-U>KittyNavigateDown<cr>
-  noremap <silent> <c-k> :<C-U>KittyNavigateUp<cr>
-  noremap <silent> <c-l> :<C-U>KittyNavigateRight<cr>
-  noremap <silent> <c-\> :<C-U>KittyNavigatePrevious<cr>
-]])
-
+-- -- TODO: I'm guessing I'd need to actually install vim-kitty-navigator for this to work...?
 -- -- see: https://github.com/christoomey/vim-tmux-navigator/blob/master/plugin/tmux_navigator.vim
 -- vim.cmd([[
---   noremap <silent> <c-h> :<C-U>TmuxNavigateLeft<cr>
---   noremap <silent> <c-j> :<C-U>TmuxNavigateDown<cr>
---   noremap <silent> <c-k> :<C-U>TmuxNavigateUp<cr>
---   noremap <silent> <c-l> :<C-U>TmuxNavigateRight<cr>
---   noremap <silent> <c-\> :<C-U>TmuxNavigatePrevious<cr>
+--   noremap <silent> <c-h> :<C-U>KittyNavigateLeft<cr>
+--   noremap <silent> <c-j> :<C-U>KittyNavigateDown<cr>
+--   noremap <silent> <c-k> :<C-U>KittyNavigateUp<cr>
+--   noremap <silent> <c-l> :<C-U>KittyNavigateRight<cr>
+--   noremap <silent> <c-\> :<C-U>KittyNavigatePrevious<cr>
 -- ]])
+
+-- see: https://github.com/christoomey/vim-tmux-navigator/blob/master/plugin/tmux_navigator.vim
+vim.cmd([[
+  noremap <silent> <c-h> :<C-U>TmuxNavigateLeft<cr>
+  noremap <silent> <c-j> :<C-U>TmuxNavigateDown<cr>
+  noremap <silent> <c-k> :<C-U>TmuxNavigateUp<cr>
+  noremap <silent> <c-l> :<C-U>TmuxNavigateRight<cr>
+  noremap <silent> <c-\> :<C-U>TmuxNavigatePrevious<cr>
+]])
