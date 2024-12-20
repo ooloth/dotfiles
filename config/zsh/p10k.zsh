3d7161b50ff327c817bd51090b0d4286d0126bb0
Michael Uloth <hello@michaeluloth.com>
2024-12-20 15:35:05 -0500

    p10k: non-bold directly name in prompt

--- config/zsh/p10k.zsh
+++ config/zsh/p10k.zsh
@@ -225,7 +225,7 @@
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND="#b4befe"
   # Set to true to display anchor directory segments in bold.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=false
   # Don't shorten directories that contain any of these files. They are anchors.
   local anchor_files=(
     .bzr
