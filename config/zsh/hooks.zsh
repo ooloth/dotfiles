6e48a74ce6f057e8697a9cc7387a5c4822dec943
Michael Uloth <hello@michaeluloth.com>
2025-01-27 20:29:27 -0500

    hooks: update python env vars depending on whether directory is python project

--- config/zsh/hooks.zsh
+++ config/zsh/hooks.zsh
@@ -1,8 +1,28 @@
 function chpwd() {
-  activate_venv
+  update_python_env_vars
+  activate_pyenv_venv
 }
 
-activate_venv() {
+is_python_project() {
+  if [ -d ".venv" ] || [ -f "pyproject.toml" ] || [ -f "requirements.txt" ] || ls *.py &>/dev/null; then
+    return 0 # Return true (success)
+  else
+    return 1 # Return false (failure)
+  fi
+}
+
+update_python_env_vars() {
+  if is_python_project; then
+    export PYTHONPATH="$PWD"
+    export MYPYPATH="$PWD"
+  else
+    unset VIRTUAL_ENV
+    export PYTHONPATH="$HOME"
+    export MYPYPATH="$HOME"
+  fi
+}
+
+activate_pyenv_venv() {
   local CURRENT_DIRECTORY=$(basename $PWD)
 
   # assumes venv is named after the current directory
@@ -13,20 +33,11 @@ activate_venv() {
   if [[ ! -d "$PYENV_VENV" ]]; then
     # remove all pyenv paths from PATH (see: https://stackoverflow.com/a/62950499/8802485)
     export PATH=$(echo $PATH | tr ':' '\n' | sed '/pyenv/d' | tr '\n' ':' | sed -r 's/:$/\n/')
-    unset VIRTUAL_ENV
-    export PYTHONPATH=$HOME
-    export MYPYPATH=$HOME
     return 0
   fi
 
-  # its much faster to activate the venv directly instead of using the pyenv shell integration
-  # see: https://stackoverflow.com/a/74290100/8802485
-  # see: https://stackoverflow.com/questions/45554864/why-am-i-getting-permission-denied-when-activating-a-venv
-  source "${PYENV_VENV}/bin/activate"
-
-  export PYTHONPATH=$PWD
-  export MYPYPATH=$PWD
 }
 
 # automatically activate appropriate venv when zsh first loads (called again in chpwd hook whenever cwd changes)
-activate_venv
+update_python_env_vars
+activate_pyenv_venv
