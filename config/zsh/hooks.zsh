c6be4529d82f11f74986893d9dd130edb34afa76
Michael Uloth <hello@michaeluloth.com>
2025-02-16 22:14:13 -0500

    zsh(hooks): support uv venv activation

--- config/zsh/hooks.zsh
+++ config/zsh/hooks.zsh
@@ -1,6 +1,6 @@
 function chpwd() {
   update_python_env_vars
-  activate_pyenv_venv
+  activate_venv
 }
 
 is_python_project() {
@@ -32,20 +32,28 @@ update_python_env_vars() {
   fi
 }
 
-activate_pyenv_venv() {
-  if ! have pyenv; then
+activate_venv() {
+  if ! have uv && ! have pyenv; then
     return 0
   fi
 
   local CURRENT_DIRECTORY=$(basename $PWD)
 
   # assumes venv is named after the current directory
+  local UV_VENV="${PWD}/.venv"
   local PYENV_VENV="${PYENV_ROOT}/versions/${CURRENT_DIRECTORY}"
 
-  # if there's no pyenv venv for this directory, deactivate any venv sticking from a previous directory and be done
-  if [[ ! -d "$PYENV_VENV" ]]; then
+  if [ -d "$UV_VENV" ]; then
+    local VENV="$UV_VENV"
+  else
+    local VENV="$PYENV_VENV"
+  fi
+
+  # Deactivate any venv sticking from a previous directory and be done
+  if [[ ! -d "$VENV" ]]; then
     # remove all pyenv paths from PATH (see: https://stackoverflow.com/a/62950499/8802485)
     export PATH=$(echo $PATH | tr ':' '\n' | sed '/pyenv/d' | tr '\n' ':' | sed -r 's/:$/\n/')
+    deactivate
     unset VIRTUAL_ENV
     return 0
   fi
@@ -53,9 +61,9 @@ activate_pyenv_venv() {
   # its much faster to activate the venv directly instead of using the pyenv shell integration
   # see: https://stackoverflow.com/a/74290100/8802485
   # see: https://stackoverflow.com/questions/45554864/why-am-i-getting-permission-denied-when-activating-a-venv
-  source "${PYENV_VENV}/bin/activate"
+  source "${VENV}/bin/activate"
 }
 
 # automatically activate appropriate venv when zsh first loads (called again in chpwd hook whenever cwd changes)
 update_python_env_vars
-activate_pyenv_venv
+activate_venv
