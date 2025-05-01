8dfe2842b96ad34d4f15f721750f7d630166acf8
Michael Uloth <hello@michaeluloth.com>
2025-05-01 16:11:48 -0400

    visidata: add config

--- config/visidata/config.py
+++ config/visidata/config.py
@@ -1,1 +1,23 @@
+# Visidata configuration file
+# See: https://www.visidata.org/docs/customize/
+# See: https://www.visidata.org/docs/api/commands
+
+# TODO: disable basedpyright and ruff
+
 options.default_width = 20
+
+Sheet.addCommand(
+    "^D",
+    "scroll-halfpage-down",
+    "cursorDown(nScreenRows//2); sheet.topRowIndex += nScreenRows//2",
+)
+
+Sheet.addCommand(
+    "^U",
+    "scroll-halfpage-up",
+    "cursorDown(nScreenRows//2); sheet.bottomRowIndex += nScreenRows//2",
+)
+
+# Enter insert mode with "i" instead of "e" to edit a cell
+TableSheet.unbindkey("i")
+TableSheet.bindkey("i", "edit-cell")
