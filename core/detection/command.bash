f9a5c55ad798bc7d8ba8b643ff928af8747a1450
Michael Uloth <hello@michaeluloth.com>
2025-08-18 23:09:25 -0400

    detection: add command detection

---
+++ core/detection/command.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+have() {
+  if [ -z "$1" ]; then
+    echo "Usage: have <command, alias or function>"
+    return 1 # false
+  fi
+
+  # Check if a command, alias or function with the provided name exists
+  if type "$1" &>/dev/null; then
+    return 0 # true
+  else
+    return 1 # false
+  fi
+}
+
+is_command_available() {
+  # Check if a command is available in the PATH
+  local cmd="$1"
+  command -v "$cmd" >/dev/null 2>&1
+  return $?
+}
