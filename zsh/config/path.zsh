da8f0d435066e8a976858c81a1e515c38228b85c
Michael Uloth <hello@michaeluloth.com>
2025-08-20 22:23:42 -0400

    tmux: move config to tmux/config

--- zsh/config/path.zsh
+++ zsh/config/path.zsh
@@ -1,10 +1,5 @@
 # PATH configuration (defined separately so it can be sourced by .zprofile)
 
-# # Deno
-# if $IS_AIR; then
-#   export PATH="/opt/homebrew/bin/deno:$PATH"
-# fi
-
 # Default
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:${PATH}" # Default system paths
 export PATH="/opt/local/bin:/opt/local/sbin:${PATH}" # Add MacPorts to PATH
