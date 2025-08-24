e8d53fdef2ae656571b9dd85e06d436504ae329e
Michael Uloth <hello@michaeluloth.com>
2025-08-24 14:00:27 -0400

    new-feature: add /shell subfolder

---
+++ @common/templates/new-feature/shell/setup.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+# TODO: zsh/plugins.zsh should find all shell/setup.zsh files and source them
---
+++ @common/templates/new-feature/shell/variables.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+# TODO: zsh/variables.zsh should find all shell/variables.zsh files and source them
