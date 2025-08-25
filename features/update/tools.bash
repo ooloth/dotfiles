34eee829f517bde65c3b6db5772d51d3580f6a92
Michael Uloth <hello@michaeluloth.com>
2025-08-25 16:47:36 -0400

    update: detect all update.bash files

--- features/update/tools.bash
+++ features/update/tools.bash
@@ -6,7 +6,16 @@ source "${DOTFILES}/tools/bash/utils.bash"
 main() {
   source "${DOTFILES}/features/update/bash/mode.bash"
 
+  # Find all update.bash files
+  update_files=$(find "${DOTFILES}/tools" -type f -name "update.bash")
+
   # TODO: find all {features,tools}/**/update.*.bash and source them all automatically?
+  # Iterate over the files and execute them
+  for file in $install_files; do
+    printf "🔄 Running %s\n" "$file"
+    printf "Careful! Enable actual command only if you're sure.\n"
+    # bash "$file"
+  done
 }
 
 # TODO: support updating individual tools?
