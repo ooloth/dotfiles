70b9c22675ff20bedcadd0e21f0c8608956d613e
Michael Uloth <hello@michaeluloth.com>
2025-08-25 19:11:02 -0400

    update: source update.bash files instead of executing them so they inherit env vars

--- features/update/tools.bash
+++ features/update/tools.bash
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source "${DOTFILES}/tools/bash/utils.bash"
-
 main() {
   source "${DOTFILES}/features/update/bash/mode.bash"
-
-  info "🧰 Updating tools..."
 
   # Find all update.bash files at the root level of each tool directory except @new and @archive
   update_files=$(find "${DOTFILES}/tools" -maxdepth 2 -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -name "update.bash" -print)
 
-  # Iterate over the files and execute them
+  # Execute each update.bash file in the current shell
   for file in $update_files; do
-    printf "🔄 Running %s\n" "$file"
-    bash "$file"
+    source "$file"
   done
 }
 
