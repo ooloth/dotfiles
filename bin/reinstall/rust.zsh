a940c52431720866cd759d48a2d4d6445b04919d
Michael Uloth <hello@michaeluloth.com>
2025-08-25 10:55:07 -0400

    features: delete "reinstall"

--- bin/reinstall/rust.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-DOTBIN="$HOME/Repos/ooloth/dotfiles/bin"
-
-source "$DOTBIN/uninstall/rust.zsh"
-source "$DOTBIN/install/rust.zsh"
--- bin/reinstall/tmux.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-DOTBIN="$HOME/Repos/ooloth/dotfiles/bin"
-
-source "$DOTBIN/uninstall/tmux.zsh"
-source "$DOTBIN/install/tmux.zsh"
