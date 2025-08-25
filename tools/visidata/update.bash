565770d50b91f9137e63941b851e60591edb7917
Michael Uloth <hello@michaeluloth.com>
2025-08-25 19:59:25 -0400

    visidata: report version when upgrading

--- tools/visidata/update.bash
+++ tools/visidata/update.bash
@@ -6,8 +6,24 @@ set -euo pipefail
 # TODO: Symlink configuration files (overkill? might as well?)
 # TODO: Validate configuration (e.g. options are still valid)
 
+source "${DOTFILES}/tools/bash/utils.bash"
+
+get_version() {
+  local version
+  version="$(vd -v)"
+  printf "${version#saul.pw/VisiData v}"
+}
+
+current_version="$(get_version)"
+
 info "📊 Updating visidata"
 
 uv tool upgrade visidata
 
-debug "🚀 Visidata is up to date"
+new_version="$(get_version)"
+
+if [ "$current_version" != "$new_version" ]; then
+  debug "🚀 Visidata updated from version $current_version to $new_version"
+else
+  debug "🚀 Visidata $new_version is already up to date"
+fi
