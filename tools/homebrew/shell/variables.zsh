942c422c294dd8fbebf88f646ab2071f9215e7c0
Michael Uloth <hello@michaeluloth.com>
2025-08-26 12:06:42 -0400

    homebrew: don't clean up after individual install/upgrade commands

---
+++ tools/homebrew/shell/variables.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+export HOMEBREW_NO_INSTALL_CLEANUP=1  # don't auto-remove old versions after every install/upgrade
--- tools/zsh/config/variables.zsh
+++ tools/zsh/config/variables.zsh
@@ -2,15 +2,34 @@
 # See: https://news.ycombinator.com/item?id=39508793
 # See: https://zsh.sourceforge.io/Doc/Release/Files.html
 
-##########################
-# PREREQUISITE VARIABLES #
-##########################
+#################
+# PREREQUISITES #
+#################
 
 source "${DOTFILES}/tools/macos/shell/variables.zsh" # sets COMPUTER
 source "${DOTFILES}/tools/zsh/config/path.zsh" # sets PATH
 
-# Editor
-EDITOR=$(command -v nvim || command -v vim || command -v vi || command -v code)
+###########
+# GENERAL #
+###########
+
+export EDITOR=$(command -v nvim || command -v vim || command -v vi || command -v code)
+export SHELL=$(which zsh)
+export XDG_CONFIG_HOME="${HOME}/.config"
+
+#########
+# TOOLS #
+#########
+
+# TODO: make dynamic
+
+source "${DOTFILES}/tools/homebrew/shell/variables.zsh"
+
+# Zsh: History
+export HISTFILE="${HOME}/.zsh_history"
+export HISTORY_IGNORE="git*"
+export HISTSIZE=100000
+export SAVEHIST=$HISTSIZE
 
 # Eza
 # see: https://github.com/eza-community/eza/blob/main/man/eza.1.md#environment-variables
@@ -19,15 +38,6 @@ export EZA_GRID_ROWS=10
 export EZA_ICON_SPACING=2
 export EZA_STRICT=true
 export TIME_STYLE=long-iso
-
-# History
-export HISTFILE="${HOME}/.zsh_history"
-export HISTORY_IGNORE="git*"
-export HISTSIZE=100000
-export SAVEHIST=$HISTSIZE
-
-# Homebrew
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # k9s
 export K9SCONFIG=$HOME/.config/k9s
@@ -44,22 +54,14 @@ export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig"
 export POWERLEVEL9K_CONFIG_FILE="${HOME}/.config/powerlevel10k/p10k.zsh"
 
 # Python
-export MYPYPATH="$HOME"
-# see: https://github.com/recursionpharma/data-science-onboarding#cloning-some-internal-repos
-export PYTHONPATH="$HOME"
-# Avoid extra venv at beginning of prompt (see: https://stackoverflow.com/a/72715907/8802485)
-export VIRTUAL_ENV_PROMPT=''
+export MYPYPATH="${HOME}"
+export PYTHONPATH="${HOME}"
+export VIRTUAL_ENV_PROMPT='' # avoid extra (venv) prompt prefix
 
 # Rust
 export CARGO_HOME="$HOME/.config/cargo"
 export RUSTUP_HOME="$HOME/.config/rustup"
 source "$CARGO_HOME/env"
-
-# Shell
-export SHELL=$(which zsh)
-
-# see: https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md#user-config
-export XDG_CONFIG_HOME=$HOME/.config
 
 if is_work; then
   source "${DOTFILES}/tools/zsh/config/work/variables.zsh" 2>/dev/null
