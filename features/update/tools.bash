8cde397911da33351ceb1c9cf22034fea86fd99a
Michael Uloth <hello@michaeluloth.com>
2025-08-25 21:16:51 -0400

    update: execute scripts instead of sourcing them

--- features/update/tools.bash
+++ features/update/tools.bash
@@ -7,9 +7,9 @@ main() {
   # Find all update.bash files at the root level of each tool directory except @new and @archive
   update_files=$(find "${DOTFILES}/tools" -maxdepth 2 -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -name "update.bash" -print)
 
-  # Execute each update.bash file in the current shell
+  # Execute each update.bash file
   for file in $update_files; do
-    source "$file"
+    bash "$file"
   done
 }
 
