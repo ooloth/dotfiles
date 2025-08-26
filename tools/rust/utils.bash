62cef3c1cc168a9af65fe91f65846a6519707fd8
Michael Uloth <hello@michaeluloth.com>
2025-08-26 18:57:25 -0400

    rust: define utils

---
+++ tools/rust/utils.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export TOOL_LOWER="rust"
+export TOOL_UPPER="Rust"
+export TOOL_COMMAND="rustup"
+export TOOL_PACKAGE="rustup"
+export TOOL_EMOJI="🦀"
+export TOOL_CONFIG_DIR="${HOME}/.config/${TOOL_LOWER}"
+
+parse_version() {
+  local raw_version="${1}"
+  local second_word="$(printf "${raw_version}" | awk '{print $2}')"
+
+  printf "${second_word}"
+}
