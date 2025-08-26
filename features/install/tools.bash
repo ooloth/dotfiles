57726a856540f98704395b3ac6c9a31b8d65e7d5
Michael Uloth <hello@michaeluloth.com>
2025-08-25 21:54:52 -0400

    install: run priority files before others

--- features/install/tools.bash
+++ features/install/tools.bash
@@ -4,8 +4,22 @@ set -euo pipefail
 DOTFILES="${HOME}/Repos/ooloth/dotfiles"
 
 main() {
+  priority_files=(
+    # TODO: populate with files that need to run first
+  )
+
+  # Run priority files first
+  for file in "${priority_files[@]}"; do
+    bash "$file"
+  done
+
   # Find all install.bash files at the root level of each tool directory except @new and @archive
   install_files=$(find "${DOTFILES}/tools" -maxdepth 2 -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -name "install.bash" -print)
+
+  # Filter out priority files from the list
+  for priority in "${priority_files[@]}"; do
+    install_files=$(echo "$install_files" | grep -v "^$priority$")
+  done
 
   # Execute each install.bash file
   for file in $install_files; do
@@ -15,4 +29,5 @@ main() {
   done
 }
 
+# TODO: support installing individual tools?
 main "$@"
