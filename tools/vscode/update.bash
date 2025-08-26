b630b93c7c0b519498d53a6f7d092fa4eb1d21a2
Michael Uloth <hello@michaeluloth.com>
2025-08-26 17:09:12 -0400

    chore: delete unused code

--- tools/vscode/update.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# TODO: update vscode if appropriate
-# TODO: install missing vscode extensions if appropriate
-# TODO: what do about installed extensions that aren't in extensions.txt?
-# TODO: update vscode extensions if appropriate
--- tools/zsh/symlinks/unlink.bash
+++ tools/zsh/symlinks/unlink.bash
@@ -6,8 +6,6 @@ source "${DOTFILES}/tools/bash/utils.bash"
 
 debug "🔗 Removing symlinked config files"
 
-# TODO: confirm this removes the files safely (not just the symlinks)
-# TODO: would I ever want to do this for zsh?
 rm "${TOOL_CONFIG_DIR}/.hushlogin"
 rm "${TOOL_CONFIG_DIR}/.zshenv"
 rm "${TOOL_CONFIG_DIR}/.zshrc"
