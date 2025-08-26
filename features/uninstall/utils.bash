1cb437cf649924daa2532d0f200e7aeb148d1628
Michael Uloth <hello@michaeluloth.com>
2025-08-26 12:03:53 -0400

    uninstall: improve post-uninstall validation

--- features/uninstall/utils.bash
+++ features/uninstall/utils.bash
@@ -4,25 +4,25 @@ set -euo pipefail
 source "${DOTFILES}/tools/bash/utils.bash"
 
 uninstall_and_unlink() {
-  local tool_lower="$1"
-  local tool_upper="$2"
-  local tool_emoji="$3"
-  local uninstall_command="$4"
-  local unlink_script_path="$5"
+  local tool_lower="${1}"
+  local tool_upper="${2}"
+  local tool_emoji="${3}"
+  local uninstall_command="${4}"
+  local unlink_script_path="${5}"
 
   info "${tool_emoji} Uninstalling ${tool_lower}"
 
-  if ! have "$tool_lower"; then
+  if ! have "${tool_lower}"; then
     printf "✅ ${tool_upper} is already uninstalled\n"
   else
-    bash -c "$uninstall_command"
+    bash -c "${uninstall_command}"
   fi
 
   # Unlink config files
-  source "$unlink_script_path"
+  source "${unlink_script_path}"
 
   # Confirm uninstallation
-  exec "${SHELL}" -l
+  "${SHELL}" -lc "exit"
 
   if have "${tool_lower}"; then
     error "❌ ${tool_lower} command still found"
