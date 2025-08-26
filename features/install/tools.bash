d0705ed08b1eb9c7d785c61879bcda103336a419
Michael Uloth <hello@michaeluloth.com>
2025-08-25 21:16:59 -0400

    install: execute scripts instead of sourcing them

--- features/install/tools.bash
+++ features/install/tools.bash
@@ -7,11 +7,11 @@ main() {
   # Find all install.bash files at the root level of each tool directory except @new and @archive
   install_files=$(find "${DOTFILES}/tools" -maxdepth 2 -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -name "install.bash" -print)
 
-  # Execute each install.bash file in the current shell
+  # Execute each install.bash file
   for file in $install_files; do
     printf "🔄 Running %s\n" "$file"
     printf "Careful! Enable actual command only if you're sure.\n"
-    # source "$file"
+    # bash "$file"
   done
 }
 
