471c5e51a41438384a3374997bb0550992c27f6a
Michael Uloth <hello@michaeluloth.com>
2025-08-26 00:49:56 -0400

    uv: extract tool name constants

--- tools/uv/update.bash
+++ tools/uv/update.bash
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-tool_lower="uv"
-tool_upper="uv"
-
-source "${DOTFILES}/tools/${tool_lower}/utils.bash"
+source "${DOTFILES}/tools/uv/utils.bash"
 source "${DOTFILES}/features/update/utils.bash"
 
 update_or_install_and_symlink \
-  "${tool_lower}" \
-  "${tool_upper}" \
-  "🐍" \
+  "${TOOL_LOWER}" \
+  "${TOOL_UPPER}" \
+  "${TOOL_EMOJI}" \
   "uv self update" \
-  "${DOTFILES}/tools/${tool_lower}/symlinks/link.bash" \
-  "${DOTFILES}/tools/${tool_lower}/install.bash" \
-  "${tool_lower} --version" \
+  "${DOTFILES}/tools/${TOOL_LOWER}/symlinks/link.bash" \
+  "${DOTFILES}/tools/${TOOL_LOWER}/install.bash" \
+  "${TOOL_LOWER} --version" \
   "parse_version"
--- tools/uv/utils.bash
+++ tools/uv/utils.bash
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+readonly TOOL_LOWER="uv"
+readonly TOOL_UPPER="uv"
+readonly TOOL_EMOJI="🐍"
+
+export TOOL_EMOJI TOOL_LOWER TOOL_UPPER
+
 parse_version() {
   # Grab the second word
   local raw_version="$1"
