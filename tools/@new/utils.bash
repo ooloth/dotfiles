1ada7c87f4e183e75d426a2fde2916ad8b746071
Michael Uloth <hello@michaeluloth.com>
2025-08-26 18:56:27 -0400

    tools: fix second word version parsing

--- tools/@new/utils.bash
+++ tools/@new/utils.bash
@@ -11,14 +11,14 @@ export TOOL_CONFIG_DIR="${HOME}/.config/${TOOL_LOWER}"
 parse_version() {
   local raw_version="${1}"
   local prefix_brew_formula="${TOOL_PACKAGE} "
-  local prefix_uv_tool="$(printf "${raw_version}" | awk '{print $2}')"
+  local second_word="$(printf "${raw_version}" | awk '{print $2}')"
   local prefix="${TOOL_PACKAGE}, version "
 
   # Grab everything after the prefix
   printf "${raw_version#"${prefix_brew_formula}"}"
 
   # Grab the second word
-  printf "${raw_version#"${prefix_uv_tool}"}"
+  printf "${second_word}"
 
   # Grab everything after the prefix on the first line only
   printf "${raw_version#"${prefix}"}" | head -n 1
--- tools/uv/utils.bash
+++ tools/uv/utils.bash
@@ -10,8 +10,7 @@ export TOOL_CONFIG_DIR="${HOME}/.config/${TOOL_LOWER}"
 
 parse_version() {
   local raw_version="${1}"
-  local prefix_uv_tool="$(printf "${raw_version}" | awk '{print $2}')"
+  local second_word="$(printf "${raw_version}" | awk '{print $2}')"
 
-  # Grab the second word
-  printf "${raw_version#"${prefix_uv_tool}"}"
+  printf "${second_word}"
 }
