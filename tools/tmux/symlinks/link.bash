08258f4a33f8a583267c31f35bbc4b13a5a711aa
Michael Uloth <hello@michaeluloth.com>
2025-08-26 17:08:58 -0400

    tmux: fix typo

--- tools/tmux/symlinks/link.bash
+++ tools/tmux/symlinks/link.bash
@@ -4,8 +4,8 @@ set -euo pipefail
 source "${DOTFILES}/tools/tmux/utils.bash"
 source "${DOTFILES}/tools/bash/utils.bash"
 
-symlink "${DOTFILES}/tools/${TMUX_LOWER}/config/battery.sh" "${TOOL_CONFIG_DIR}"
-symlink "${DOTFILES}/tools/${TMUX_LOWER}/config/gitmux.conf" "${TOOL_CONFIG_DIR}"
-symlink "${DOTFILES}/tools/${TMUX_LOWER}/config/tmux.conf" "${TOOL_CONFIG_DIR}"
-symlink "${DOTFILES}/tools/${TMUX_LOWER}/config/tmux.terminfo" "${TOOL_CONFIG_DIR}"
-symlink "${DOTFILES}/tools/${TMUX_LOWER}/config/xterm-256color-italic.terminfo" "${TOOL_CONFIG_DIR}"
+symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/battery.sh" "${TOOL_CONFIG_DIR}"
+symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/gitmux.conf" "${TOOL_CONFIG_DIR}"
+symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/tmux.conf" "${TOOL_CONFIG_DIR}"
+symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/tmux.terminfo" "${TOOL_CONFIG_DIR}"
+symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/xterm-256color-italic.terminfo" "${TOOL_CONFIG_DIR}"
