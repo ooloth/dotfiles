88c5377ff38b8733b453bf312c535d1d545aa486
Michael Uloth <hello@michaeluloth.com>
2025-08-27 15:13:50 -0400

    docs: clean up comments

--- tools/gemini/shell/integration.zsh
+++ tools/gemini/shell/integration.zsh
@@ -1,3 +1,1 @@
 #!/usr/bin/env zsh
-
-# TODO: zsh/plugins.zsh should find all shell/integration.zsh files and source them
--- tools/television/shell/integration.zsh
+++ tools/television/shell/integration.zsh
@@ -5,9 +5,7 @@ source "${DOTFILES}/tools/zsh/utils.zsh"
 # TODO: save and source them instead? which is more performant?
 # see: https://alexpasmantier.github.io/television/docs/Users/shell-integration#customizing-shell-integration-scripts
 
-# Enables:
-#   - ctrl-r = command history
-#   - ctrl-t = command smart autocompletion
+# Enables ctrl-r for command history
 # See: https://alexpasmantier.github.io/television/docs/Users/shell-integration
 if have tv; then
   eval "$(tv init zsh)"
