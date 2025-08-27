bae52af37f7a57f7578942a47e869ef511b3f657
Michael Uloth <hello@michaeluloth.com>
2025-08-27 14:50:38 -0400

    update: more explicitly update one tool or all tools

--- features/update/shell/aliases.zsh
+++ features/update/shell/aliases.zsh
@@ -6,6 +6,12 @@ alias symlinks="bash ${DOTFILES}/features/update/symlinks.bash"
 u() {
   local tool="$1"
 
-  bash "${DOTFILES}/features/update/tools.bash" "$tool";
-  zsh "${DOTFILES}/features/update/tools.zsh";
+  if [[ ! -z "$tool" ]]; then
+    # If a specific tool was provided, jsut update that tool
+    bash "${DOTFILES}/features/update/tools.bash" "$tool";
+  else
+    # Otherwise, update all tools
+    bash "${DOTFILES}/features/update/tools.bash";
+    zsh "${DOTFILES}/features/update/tools.zsh";
+  fi
 }
