d1c7018b84a839aa7508e08437668d47fa385d32
Michael Uloth <hello@michaeluloth.com>
2025-08-27 13:15:30 -0400

    zsh: include is_air, is_mini, is_work helpers in utils

--- tools/zsh/utils.zsh
+++ tools/zsh/utils.zsh
@@ -1,3 +1,11 @@
+#!/usr/bin/env zsh
+
+# In case this file is sourced before shell variables have been symlinked
+export DOTFILES="${HOME}/Repos/ooloth/dotfiles"
+
+# Include is_air, is_mini, is_work in this commonly-sourced file
+source "${DOTFILES}/tools/macos/shell/aliases.zsh"
+
 ##############
 # INSPECTING #
 ##############
