6fc14e37a5bf8840b45ac5067ac665c1bf0188bb
Michael Uloth <hello@michaeluloth.com>
2025-08-27 13:12:06 -0400

    install: report if selected tool has no install.bash file

--- features/install/tools.bash
+++ features/install/tools.bash
@@ -6,10 +6,16 @@ export DOTFILES="${HOME}/Repos/ooloth/dotfiles"
 
 main() {
   local tool="${1:-}"
+  local file_name="install.bash"
 
   # If a specific tool is provided as an argument, install only that tool
   if [[ ! -z "$tool" ]]; then
-    bash "${DOTFILES}/tools/${tool}/install.bash"
+    if [[ ! -f "${DOTFILES}/tools/${tool}/${file_name}" ]]; then
+      echo "❌ No ${file_name} script found for tool: ${tool}"
+      return 1
+    fi
+
+    bash "${DOTFILES}/tools/${tool}/${file_name}"
     return 0
   fi
 
@@ -24,11 +30,11 @@ main() {
   done
 
   # Find all install.bash files at the root level of each tool directory (except @new and @archive) and sort by parent directory name
-  install_files=$(find "${DOTFILES}/tools" -maxdepth 2 -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -name "install.bash" -print | sort -t/ -k5)
+  install_files=$(find "${DOTFILES}/tools" -maxdepth 2 -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -name "${file_name}" -print | sort -t/ -k5)
 
   # Filter out priority files from the list
-  for priority in "${priority_files[@]}"; do
-    install_files=$(echo "$install_files" | grep -v "^$priority$")
+  for file in "${priority_files[@]}"; do
+    install_files=$(echo "$install_files" | grep -v "^$file$")
   done
 
   # Execute each install.bash file
