fc4e1efe23afb42c14575147e19478eb4d254f3a
Michael Uloth <hello@michaeluloth.com>
2025-08-27 14:09:12 -0400

    tmux: remove battery level script

--- tools/tmux/config/battery.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo "♥" "$(pmset -g batt | grep -Eo '[0-9]+%')"
--- tools/tmux/symlinks/link.bash
+++ tools/tmux/symlinks/link.bash
@@ -4,6 +4,5 @@ set -euo pipefail
 source "${DOTFILES}/tools/tmux/utils.bash"
 source "${DOTFILES}/tools/bash/utils.bash"
 
-symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/battery.sh" "${TOOL_CONFIG_DIR}"
 symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/gitmux.conf" "${TOOL_CONFIG_DIR}"
 symlink "${DOTFILES}/tools/${TOOL_LOWER}/config/tmux.conf" "${TOOL_CONFIG_DIR}"
