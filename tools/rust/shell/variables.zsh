d3c8e40363450221003c6aba6a48c49bdd325035
Michael Uloth <hello@michaeluloth.com>
2025-08-27 12:26:58 -0400

    rust: ensure executables are always on PATH

--- tools/rust/shell/variables.zsh
+++ tools/rust/shell/variables.zsh
@@ -1,13 +1,7 @@
 #!/usr/bin/env zsh
 
-source "${DOTFILES}/tools/zsh/utils.zsh"
-
 # See: https://rust-lang.github.io/rustup/environment-variables.html
 export CARGO_HOME="${HOME}/.config/cargo"
 export RUSTUP_HOME="${HOME}/.config/rustup"
-
-if have rustup; then
-  export PATH="${CARGO_HOME}/bin:${PATH}"
-  source "${CARGO_HOME}/env"
-fi
+export PATH="${CARGO_HOME}/bin:${PATH}"
 
