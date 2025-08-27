d14ed59d7ae107b9b2d4f54562099ecbdb294d14
Michael Uloth <hello@michaeluloth.com>
2025-08-27 12:55:38 -0400

    neovim: update last since slowest for now

--- features/update/tools.zsh
+++ features/update/tools.zsh
@@ -4,17 +4,11 @@ set -euo pipefail
 # In case this file is sourced before shell variables have been symlinked
 export DOTFILES="${HOME}/Repos/ooloth/dotfiles"
 
-source "${DOTFILES}/tools/zsh/utils.zsh"
+# TODO: replace all of these with tool/<tool>/update.bash files
+source "${DOTFILES}/features/update/zsh/gcloud.zsh"
+source "${DOTFILES}/features/update/zsh/homebrew.zsh"
+source "${DOTFILES}/features/update/zsh/npm.zsh"
+source "${DOTFILES}/features/update/zsh/neovim.zsh"
 
-main() {
-  source "${DOTFILES}/features/update/zsh/neovim.zsh"
-  source "${DOTFILES}/features/update/zsh/npm.zsh"
-  source "${DOTFILES}/features/update/zsh/gcloud.zsh"
-  source "${DOTFILES}/features/update/zsh/homebrew.zsh"
-
-  info "🐚 Reloading zsh"
-  exec -l "${SHELL}"
-}
-
-# TODO: support updating individual tools?
-main "$@"
+printf "\n🔁 Reloading shell"
+exec -l "${SHELL}"
