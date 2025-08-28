eada9840b69395e4158bdfd5150c98bf4476e0f8
Michael Uloth <hello@michaeluloth.com>
2025-08-28 00:32:40 -0400

    homebrew: extract PATH update

--- tools/homebrew/shell/variables.zsh
+++ tools/homebrew/shell/variables.zsh
@@ -2,6 +2,7 @@
 
 source "${DOTFILES}/tools/zsh/utils.zsh"
 
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
 export HOMEBREW_NO_AUTO_UPDATE=1      # I'll update manually (don't slow down individual install/upgrade commands)
 export HOMEBREW_NO_INSTALL_CLEANUP=1  # I'll clean up manually (don't slow down individual install/upgrade commands)
 
