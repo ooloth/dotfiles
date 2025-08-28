3d11be08a12a072a07f7e820d3916536a8e94338
Michael Uloth <hello@michaeluloth.com>
2025-08-28 13:36:44 -0400

    visidata: fix order of update args

--- tools/visidata/update.bash
+++ tools/visidata/update.bash
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source "${DOTFILES}/tools/visidata/utils.bash"
 source "${DOTFILES}/features/update/utils.bash"
+source "${DOTFILES}/tools/visidata/utils.bash"
 
 update_and_symlink \
   "${TOOL_LOWER}" \
   "${TOOL_UPPER}" \
   "${TOOL_COMMAND}" \
   "${TOOL_EMOJI}" \
+  "uv tool upgrade ${TOOL_PACKAGE}" \
   "${TOOL_PACKAGE} --version" \
   "parse_version" \
-  "uv tool upgrade ${TOOL_PACKAGE}" \
   "${DOTFILES}/tools/${TOOL_LOWER}/install.bash" \
   "${DOTFILES}/tools/${TOOL_LOWER}/symlinks/link.bash"
