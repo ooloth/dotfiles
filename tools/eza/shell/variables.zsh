2cde725698aeccc4726eefa294ea1eb8f596b9e6
Michael Uloth <hello@michaeluloth.com>
2025-08-28 00:05:49 -0400

    eza: migrate environment variables to tools folder

---
+++ tools/eza/shell/variables.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# see: https://github.com/eza-community/eza/blob/main/man/eza.1.md#environment-variables
+# see: https://github.com/eza-community/eza/blob/main/src/options/vars.rs
+export EZA_GRID_ROWS=10
+export EZA_ICON_SPACING=2
+export EZA_STRICT=true
+export TIME_STYLE=long-iso
--- tools/zsh/config/variables.zsh
+++ tools/zsh/config/variables.zsh
@@ -41,14 +41,6 @@ export HISTORY_IGNORE="git*"
 export HISTSIZE=100000
 export SAVEHIST=$HISTSIZE
 
-# Eza
-# see: https://github.com/eza-community/eza/blob/main/man/eza.1.md#environment-variables
-# see: https://github.com/eza-community/eza/blob/main/src/options/vars.rs
-export EZA_GRID_ROWS=10
-export EZA_ICON_SPACING=2
-export EZA_STRICT=true
-export TIME_STYLE=long-iso
-
 # k9s
 export K9SCONFIG=$HOME/.config/k9s
 
