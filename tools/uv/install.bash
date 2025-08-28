24b3f6612e090bea31812eb12695b8fcb5b255af
Michael Uloth <hello@michaeluloth.com>
2025-08-28 13:43:36 -0400

    uv: use different install command at work

--- tools/uv/install.bash
+++ tools/uv/install.bash
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source "${DOTFILES}/tools/uv/utils.bash"
 source "${DOTFILES}/features/install/utils.bash"
+source "${DOTFILES}/tools/uv/utils.bash"
 
-# See: https://docs.astral.sh/uv/getting-started/installation/
 install_and_symlink \
   "${TOOL_LOWER}" \
   "${TOOL_UPPER}" \
   "${TOOL_COMMAND}" \
   "${TOOL_EMOJI}" \
-  "curl -LsSf https://astral.sh/uv/install.sh | sh" \
+  "${TOOL_INSTALL_COMMAND}" \
   "${TOOL_PACKAGE} --version" \
   "parse_version" \
   "${DOTFILES}/tools/${tool_lower}/symlinks/link.bash"
--- tools/uv/utils.bash
+++ tools/uv/utils.bash
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+source "${DOTFILES}/tools/bash/utils.bash"
+
 export TOOL_LOWER="uv"
 export TOOL_UPPER="uv"
 export TOOL_COMMAND="uv"
 export TOOL_PACKAGE="uv"
 export TOOL_EMOJI="🐍"
 export TOOL_CONFIG_DIR="${HOME}/.config/${TOOL_LOWER}"
+
+# See: https://docs.astral.sh/uv/getting-started/installation/
+export TOOL_INSTALL_COMMAND="curl -LsSf https://astral.sh/uv/install.sh | sh"
+
+if is_work; then
+  debug "⚠️ Using work's custom ${TOOL_LOWER} install command"
+  # See: https://python.prod.rxrx.io/UV-Adoption-Guide
+  export TOOL_INSTALL_COMMAND="curl -LsSf https://python.prod.rxrx.io/rxrx-setup-uv.sh | sh"
+fi
 
 parse_version() {
   local raw_version="${1}"
