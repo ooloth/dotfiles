0802fa8add3b6ae117801a361876a8c3bbc12a27
Michael Uloth <hello@michaeluloth.com>
2025-08-29 21:13:36 -0400

    zsh: don't alias grep to rg

--- tools/zsh/config/aliases.zsh
+++ tools/zsh/config/aliases.zsh
@@ -47,7 +47,6 @@ diff() { kitten diff "$1" "$2"; }                                   # see: https
 alias f='yazi'
 
 alias g="lazygit"
-alias grep="rg"
 
 alias image="kitten icat" # see: https://sw.kovidgoyal.net/kitty/kittens/icat/
 
