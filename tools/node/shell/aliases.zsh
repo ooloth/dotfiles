b9d5d23133f259be0b3d2852fdc90c1c7ac422b4
Michael Uloth <hello@michaeluloth.com>
2025-09-01 14:31:55 -0400

    node: add "ng" aliases for "npm -g"

--- tools/node/shell/aliases.zsh
+++ tools/node/shell/aliases.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 
+# Global npm dependencies for current node version
 alias ng="zsh ${DOTFILES}/tools/node/update.bash" # updated global npm dependencies for current node version
-alias nu="npm i && npm outdated" # check for outdated project dependencies
+alias ngl="npm -g list"
+alias ngo="npm -g outdated"
+alias ngu="npm -g uninstall"
+
+alias no="npm i && npm outdated" # check for outdated project dependencies
