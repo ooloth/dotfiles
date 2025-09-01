364d8f2c3860c1d53a91b99bebc7e39242f4db07
Michael Uloth <hello@michaeluloth.com>
2025-09-01 13:36:34 -0400

    vscode: delete unused install/update commands

--- tools/vscode/install.bash
+++ tools/vscode/install.bash
@@ -4,16 +4,6 @@ set -euo pipefail
 source "${DOTFILES}/features/install/utils.bash"
 source "${DOTFILES}/tools/vscode/utils.bash" # source last to avoid env var overrides
 
-# install_and_symlink \
-#   "${TOOL_LOWER}" \
-#   "${TOOL_UPPER}" \
-#   "${TOOL_COMMAND}" \
-#   "${TOOL_EMOJI}" \
-#   "brew install --cask ${TOOL_PACKAGE}" \
-#   "brew list --version ${TOOL_PACKAGE}" \
-#   "parse_version" \
-#   "${DOTFILES}/tools/${TOOL_LOWER}/symlinks/link.bash"
-
 info "${TOOL_EMOJI} Installing VS Code and its extensions"
 brew bundle --file="${DOTFILES}/tools/${TOOL_LOWER}/Brewfile"
 
@@ -21,14 +11,3 @@ debug "🔗 Symlinking VS Code configuration"
 bash "${DOTFILES}/tools/${TOOL_LOWER}/symlinks/link.bash"
 
 debug "🚀 ${TOOL_UPPER} is installed"
-
-# debug "📦 Installing extensions from extensions.txt"
-# while IFS= read -r extension; do
-#   if code --list-extensions | grep -q "${extension}"; then
-#     printf "✅ VS Code extension already installed: ${extension}\n"
-#   elif code --install-extension "$extension"; then
-#     echo "✅ Installed $extension"
-#   else
-#     echo "❌ Failed to install $extension"
-#   fi
-# done <"${DOTFILES}/tools/${TOOL_LOWER}/config/extensions.txt"
--- tools/vscode/update.bash
+++ tools/vscode/update.bash
@@ -4,17 +4,6 @@ set -euo pipefail
 source "${DOTFILES}/features/update/utils.bash"
 source "${DOTFILES}/tools/vscode/utils.bash" # source last to avoid env var overrides
 
-# update_and_symlink \
-#   "${TOOL_LOWER}" \
-#   "${TOOL_UPPER}" \
-#   "${TOOL_COMMAND}" \
-#   "${TOOL_EMOJI}" \
-#   "brew upgrade --formula ${TOOL_PACKAGE}" \
-#   "brew list --version ${TOOL_PACKAGE}" \
-#   "parse_version" \
-#   "${DOTFILES}/tools/${TOOL_LOWER}/install.bash" \
-#   "${DOTFILES}/tools/${TOOL_LOWER}/symlinks/link.bash"
-
 info "${TOOL_EMOJI} Updating VS Code and its extensions"
 brew bundle --file="${DOTFILES}/tools/${TOOL_LOWER}/Brewfile"
 
