dd4a879f157acd88904b796ab647d145ec6652ad
Michael Uloth <hello@michaeluloth.com>
2025-09-02 10:28:32 -0400

    visidata: comment out alias that may not be needed after correct installation

--- tools/visidata/shell/aliases.zsh
+++ tools/visidata/shell/aliases.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
-alias vd='uvx --with pyarrow visidata' # support opening parquet files
+# TODO: delete if it turns out to be unnecessary after installing visidata with pyarrow
+# alias vd='uvx --with pyarrow visidata' # support opening parquet files
