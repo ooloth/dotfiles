1ade3b5cf2c374a7ed536a72434c8c261cc5a3ee
Michael Uloth <hello@michaeluloth.com>
2025-11-01 13:10:21 -0400

    zsh: replace config/plugins.zsh with config/tools.zsh

--- tools/zsh/config/.zshrc
+++ tools/zsh/config/.zshrc
@@ -1,3 +1,7 @@
+##################
+# INSTANT PROMPT #
+##################
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n] confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
@@ -13,9 +17,13 @@ export DOTFILES="${HOME}/Repos/ooloth/dotfiles"
 source "${DOTFILES}/tools/macos/shell/aliases.zsh" # is_air, is_mini, is_work
 source "${DOTFILES}/tools/zsh/utils.zsh" # have, info, warn, error - TODO: deprecate in favor of bash one?
 
-source "${DOTFILES}/tools/zsh/config/variables.zsh"
-source "${DOTFILES}/tools/zsh/config/aliases.zsh"
+##########
+# CONFIG #
+##########
+
 source "${DOTFILES}/tools/zsh/config/options.zsh"
-source "${DOTFILES}/tools/zsh/config/hooks.zsh"
 source "${DOTFILES}/tools/zsh/config/completions.zsh"
-source "${DOTFILES}/tools/zsh/config/plugins.zsh" # source last
+source "${DOTFILES}/tools/zsh/config/hooks.zsh"
+source "${DOTFILES}/tools/zsh/config/variables.zsh" # legacy? or still correct for zsh itself?
+source "${DOTFILES}/tools/zsh/config/aliases.zsh" # legacy? or still correct for zsh itself?
+source "${DOTFILES}/tools/zsh/config/tools.zsh" # source last
---
+++ tools/zsh/config/tools.zsh
@@ -0,0 +1,54 @@
+# Add tool-specific environment variables, aliases and completions to zsh environment
+
+############################
+# COMPLETE SHELL.ZSH FILES #
+############################
+
+# Find all shell.zsh files in each tool directory (except @new and @archive)
+shell_files=($(find "${DOTFILES}/tools" -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -path "*/shell.zsh" -print))
+
+for file in "${shell_files[@]}"; do
+  source "${file}"
+done
+
+#####################################
+# LEGACY: SHELL/VARIABLES.ZSH FILES #
+#####################################
+
+###################################
+# LEGACY: SHELL/ALIASES.ZSH FILES #
+###################################
+
+#######################################
+# LEGACY: SHELL/INTEGRATION.ZSH FILES #
+#######################################
+
+# Find all shell/integration.bash files in each tool directory (except @new and @archive)
+shell_integration_files=($(find "${DOTFILES}/tools" -type d \( -name "@new" -o -name "@archive" \) -prune -o -type f -path "*/shell/integration.zsh" -print))
+
+for file in "${shell_integration_files[@]}"; do
+  source "${file}"
+done
+
+###################################
+# LEGACY: MANUAL COMPLETION SETUP #
+###################################
+
+# FIXME: crashes terminal if set -e is enabled
+# powerlevel10k
+# see: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#homebrew
+source "/opt/homebrew/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme"
+source "${DOTFILES}/tools/powerlevel10k/config/p10k.zsh" # to customize, run `p10k configure` or edit zsh/config/p10k.zsh
+
+# docker
+fpath=("${HOME}/.docker/completions" $fpath)
+
+if have fzf; then
+  eval "$(fzf --zsh)"
+fi
+
+if have uv; then
+  eval "$(uv generate-shell-completion zsh)"
+  eval "$(uvx --generate-shell-completion zsh)"
+fi
+
