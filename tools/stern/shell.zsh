9b6fef991adb8a477e1a1d070f415ae1103fa9da
Michael Uloth <hello@michaeluloth.com>
2025-11-01 16:02:37 -0400

    stern: migrate aliases to shell.zsh

---
+++ tools/stern/shell.zsh
@@ -0,0 +1,22 @@
+source "${DOTFILES}/tools/zsh/utils.zsh" # have, is_work
+
+########################
+# ENVIROMENT VARIABLES #
+########################
+
+###########
+# ALIASES #
+###########
+
+# Use stern as a replacement for kubectl logs
+# Tails a pod regex or "resource/name" and shows logs for any containers that match the regex
+# Add -c regex to filter by container name
+# see: https://github.com/stern/stern#usage
+if have stern; then
+  alias kl="stern"
+fi
+
+###############
+# COMPLETIONS #
+###############
+
--- tools/zsh/config/aliases.zsh
+++ tools/zsh/config/aliases.zsh
@@ -13,12 +13,6 @@ alias f='yazi'
 alias g="lazygit"
 alias grep="rg"
 
-# Use stern as a replacement for kubectl logs
-# Tails a pod regex or "resource/name" and shows logs for any containers that match the regex
-# Add -c regex to filter by container name
-# see: https://github.com/stern/stern#usage
-alias kl="stern"
-
 alias lint="check" # I forget I refer to this as "check" sometimes
 
 alias mr="sudo shutdown -r now"         # restart macos
