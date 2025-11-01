2d7386e6e58cba464b95f91c206c9c9eb46d7527
Michael Uloth <hello@michaeluloth.com>
2025-11-01 13:06:13 -0400

    tools: introduce single file for shell vars, aliases & completions

---
+++ tools/@new/shell.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+source "${DOTFILES}/tools/zsh/utils.zsh" # is_work
+
+########################
+# ENVIROMENT VARIABLES #
+########################
+
+###########
+# ALIASES #
+###########
+
+###############
+# INTEGRATION #
+###############
+
