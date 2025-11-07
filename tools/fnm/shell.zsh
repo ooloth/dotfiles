7052248bd57ecb42dbf9503131bbf6e6de7cf58f
Michael Uloth <hello@michaeluloth.com>
2025-11-07 17:19:46 -0500

    fnm: migrate to shell.zsh pattern

---
+++ tools/fnm/shell.zsh
@@ -0,0 +1,21 @@
+source "${DOTFILES}/tools/zsh/utils.zsh" # have, is_work
+
+########################
+# ENVIROMENT VARIABLES #
+########################
+
+###########
+# ALIASES #
+###########
+
+alias nvm="fnm"
+
+###############
+# COMPLETIONS #
+###############
+
+# See: https://github.com/Schniz/fnm/blob/master/docs/configuration.md
+if have fnm; then
+  eval "$(fnm env --use-on-cd --shell zsh --corepack-enabled --log-level=error)"
+fi
+
--- tools/fnm/shell/variables.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-source "${DOTFILES}/tools/zsh/utils.zsh"
-
-# See: https://github.com/Schniz/fnm/blob/master/docs/configuration.md
-if have fnm; then
-  eval "$(fnm env --use-on-cd --shell zsh --corepack-enabled --log-level=error)"
-fi
