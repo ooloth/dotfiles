0f8d8285257ad4453f6ecea0b28bbc269c7d3d97
Michael Uloth <hello@michaeluloth.com>
2025-11-07 17:19:27 -0500

    docker: vary aliases on work laptop

--- tools/docker/shell.zsh
+++ tools/docker/shell.zsh
@@ -1,5 +1,9 @@
 source "${DOTFILES}/tools/zsh/utils.zsh" # have, is_work
 
+if ! have docker; then
+  return
+fi
+
 ########################
 # ENVIROMENT VARIABLES #
 ########################
@@ -8,19 +12,22 @@ source "${DOTFILES}/tools/zsh/utils.zsh" # have, is_work
 # ALIASES #
 ###########
 
-if have docker; then
-  # Docker
-  da() { docker container ls --all --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"; }  # list all containers
-  db() { docker build . "$@"; }
-  de() { docker container exec -it "$@" sh; }
-  dp() { docker system prune "$@"; }
+# Docker
+da() { docker container ls --all --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"; }  # list all containers
+de() { docker container exec -it "$@" sh; }
+dp() { docker system prune; }  # remove all stopped containers, unused networks, dangling images, and build cache
 
-  # Docker Compose
-  dc() { docker compose "$@"; }
-  dd() { dc down "$@" }   # stop and remove one or more containers, networks, images, and volumes (or all if no args provided)
-  dl() { dc logs --follow --tail=100; }                          # see last 100 log lines of one or more services (or all services if no args provided)
-  du() { dc up --build --detach --remove-orphans "$@"; }              # recreate and start one or more services (or all services if no args provided)
+if is_work; then
+  alias db="docker build --secret id=gcp_adc,src=${HOME}/.config/gcloud/application_default_credentials.json ."
+else
+  alias db="docker build ."
 fi
+
+# Docker Compose
+dc() { docker compose "$@"; }
+dd() { dc down "$@" }   # stop and remove one or more containers, networks, images, and volumes (or all if no args provided)
+dl() { dc logs --follow --tail=100; }                          # see last 100 log lines of one or more services (or all services if no args provided)
+du() { dc up --build --detach --remove-orphans "$@"; }              # recreate and start one or more services (or all services if no args provided)
 
 ###############
 # COMPLETIONS #
