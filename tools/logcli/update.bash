1e4caa85fac805fcadb01db33061cb3f1577d076
Michael Uloth <hello@michaeluloth.com>
2025-11-19 10:25:52 -0500

    logcli: add install, update and uninstall scripts

---
+++ tools/logcli/update.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source "${DOTFILES}/features/update/utils.bash"
+source "${DOTFILES}/tools/logcli/utils.bash" # source last to avoid env var overrides
+
+update_and_symlink \
+  "${TOOL_LOWER}" \
+  "${TOOL_UPPER}" \
+  "${TOOL_COMMAND}" \
+  "${TOOL_EMOJI}" \
+  "brew upgrade --formula ${TOOL_PACKAGE}" \
+  "brew list --version ${TOOL_PACKAGE}" \
+  "parse_version" \
+  "${DOTFILES}/tools/${TOOL_LOWER}/install.bash"
