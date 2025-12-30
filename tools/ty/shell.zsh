a15f64301832c479b4bffca5d56e763be2bed5cd
Michael Uloth <hello@michaeluloth.com>
2025-12-30 15:19:46 -0500

    ty: add cli command completion

---
+++ tools/ty/shell.zsh
@@ -0,0 +1,10 @@
+
+###############
+# COMPLETIONS #
+###############
+
+if have ty; then
+  # see: https://docs.astral.sh/ty/installation/#shell-autocompletion
+  eval "$(ty generate-shell-completion zsh)"
+fi
+
