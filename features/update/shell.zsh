4265d2ccf7b6d8b9eb09870550c75d181858771b
Michael Uloth <hello@michaeluloth.com>
2026-04-18 09:25:22 -0400

    update: quote $tool variable in u() function

--- features/update/shell.zsh
+++ features/update/shell.zsh
@@ -8,9 +8,9 @@ alias symlinks="bash ${DOTFILES}/features/update/symlinks.bash"
 u() {
   local tool="$1"
 
-  if [[ ! -z "$tool" ]]; then
+  if [[ ! -z "${tool}" ]]; then
     # If a specific tool was provided, jsut update that tool
-    bash "${DOTFILES}/features/update/tools.bash" "$tool";
+    bash "${DOTFILES}/features/update/tools.bash" "${tool}";
   else
     # Otherwise, update all tools
     bash "${DOTFILES}/features/update/tools.bash";
