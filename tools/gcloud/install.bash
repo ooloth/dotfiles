582b50dc8c86f29a8069e3b66d857a76631acbdf
Michael Uloth <hello@michaeluloth.com>
2026-04-20 15:10:40 -0400

    tools/gcloud: rewrite install.bash — fix exec bug, drop utils.bash
    #131
    
    Replace `exec -l "${SHELL}"` with `source path.bash.inc` so gcloud is
    available on PATH for the remaining commands in the same script.
    Previously, exec replaced the process so `gcloud auth login` was
    unreachable. Drop the gcloud/utils.bash source and install_and_symlink
    wrapper.

--- tools/gcloud/install.bash
+++ tools/gcloud/install.bash
@@ -3,33 +3,26 @@ set -euo pipefail
 
 source "${DOTFILES}/tools/bash/utils.bash"
 
-debug "⚠️ Must install gcloud manually via https://cloud.google.com/sdk/docs/install#mac"
-
-# TODO: replace with this? - https://formulae.brew.sh/cask/gcloud-cli
-# TODO: confirm this works smoothly
+# TODO: replace with brew cask? - https://formulae.brew.sh/cask/gcloud-cli
 # See: https://cloud.google.com/sdk/docs/downloads-interactive#silent
 info "☁️ Installing gcloud SDK to ${HOME}/google-cloud-sdk"
-curl https://sdk.cloud.google.com >install.sh
+curl https://sdk.cloud.google.com > install.sh
 bash install.sh --disable-prompts
 trash install.sh
+
+# Reload PATH so gcloud is available to the remaining commands in this script
+# shellcheck source=/dev/null
+source "${HOME}/google-cloud-sdk/path.bash.inc"
 
 # See: https://cloud.google.com/kubernetes-engine/docs/how-to/cluster-access-for-kubectl#install_plugin
 info "📦 Installing required gcloud components"
 gcloud components install gke-gcloud-auth-plugin
 gcloud container clusters get-credentials prod-cluster --region us-central1 --project prod-cluster-cc74bd08
 
-# This will:
-# 1. Add credentials to your Docker config (~/.docker/config.json)
-# 2. Allow Docker to authenticate when pulling images from us-central1-docker.pkg.dev
+# Adds credentials to ~/.docker/config.json so Docker can pull from us-central1-docker.pkg.dev
 info "🐳 Configuring Docker to authenticate with gcloud"
 gcloud auth configure-docker us-central1-docker.pkg.dev
 
-# gcloud container clusters get-credentials work-cluster --region us-central1 --project work-cluster-85851b24
-# gcloud container clusters get-credentials prod-cluster --region us-central1 --project prod-cluster-cc74bd08
-
-debug "🔁 Restarting shell to load gcloud into PATH"
-exec -l "${SHELL}"
-
-debug "🔒 Authenticating with gcloud"
-# NOTE: running gcloud init is not required: https://stackoverflow.com/questions/42379685/can-i-automate-google-cloud-sdk-gcloud-init-interactive-command
+# NOTE: gcloud init is not required: https://stackoverflow.com/questions/42379685/can-i-automate-google-cloud-sdk-gcloud-init-interactive-command
+info "🔒 Authenticating with gcloud"
 gcloud auth login --update-adc
