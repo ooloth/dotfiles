0a8a043a11fc2f16157d1cb8eae0652c32b9d39f
Michael Uloth <hello@michaeluloth.com>
2026-06-04 10:18:24 -0400

    visidata: fix keymaps and ensure config is loaded
    Previously the config defined commands using the wrong class (Sheet
    instead of BaseSheet), incorrect key notation (^D/^U rather than the
    accepted format), and a broken scroll-up execstr that moved the cursor
    down and incremented bottomRowIndex instead of decrementing topRowIndex.
    
    The shell config now passes --config explicitly to the vd alias so the
    config path is guaranteed to be picked up, and uses ${HOME} instead of
    $HOME for consistency. The config itself is restructured with section
    headers, uses BaseSheet throughout, and corrects the scroll-halfpage-up
    logic.

--- tools/visidata/config/config.py
+++ tools/visidata/config/config.py
@@ -1,26 +1,42 @@
-# ruff: noqa
-# type: ignore
-
 # Visidata configuration file
 # See: https://www.visidata.org/docs/customize/
 # See: https://www.visidata.org/docs/api/commands
 
+# ruff: noqa
+# type: ignore
+
+
+###########
+# OPTIONS #
+###########
+
 options.color_default = "white on transparent"
 options.default_width = 20
+options.visidata_dir = "~/.config/visidata/"
 
-# FIXME: doesn't work yet
-Sheet.addCommand("0", "go-leftmost")
+#############################
+# KEYBINDS FOR NEW COMMANDS #
+#############################
 
-Sheet.addCommand(
-    "^D",
+# If a command already exists and I'm just changing the keybind, use "bindkey()". If it doesn't
+# exist yet, use "addCommand" to define the keybind, command name and exec string.
+
+BaseSheet.addCommand(
+    "Ctrl+D",
     "scroll-halfpage-down",
     "cursorDown(nScreenRows//2); sheet.topRowIndex += nScreenRows//2",
 )
-Sheet.addCommand(
-    "^U",
+BaseSheet.addCommand(
+    "Ctrl+U",
     "scroll-halfpage-up",
-    "cursorDown(nScreenRows//2); sheet.bottomRowIndex += nScreenRows//2",
+    "cursorDown(-nScreenRows//2); sheet.topRowIndex -= nScreenRows//2",
 )
+
+##################################
+# KEYBINDS FOR EXISTING COMMANDS #
+##################################
+
+BaseSheet.bindkey("0", "go-leftmost")
 
 # Enter insert mode with "i" instead of "e" to edit a cell
 TableSheet.unbindkey("i")
--- tools/visidata/shell.zsh
+++ tools/visidata/shell.zsh
@@ -4,11 +4,13 @@
 
 # Not sure why this is necessary for the config to be picked up, but this does work
 # Learned about this env var here: https://github.com/saulpw/visidata/issues/487#issuecomment-735643992
-export VD_CONFIG="$HOME/.config/visidata/config.py"
+export VD_CONFIG="${HOME}/.config/visidata/config.py"
 
 ###########
 # ALIASES #
 ###########
+
+alias vd='uvx visidata --config "${VD_CONFIG}"' # explicitly specify config path to ensure it is picked up
 
 # TODO: delete if it turns out to be unnecessary after installing visidata with pyarrow
 # alias vd='uvx --with pyarrow visidata' # support opening parquet files
